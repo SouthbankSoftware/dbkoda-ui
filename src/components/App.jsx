71fe52a5a81729aeed64144023663bb72277b65b
Michael Harrison <mike@southbanksoftware.com>
2017-02-28 16:59:28 +1100

    Modified .JSX from 4 spaces to 2. Added VSCode to .gitignore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default class App extends React.Component {
-    render() {
-      return (
-        <div style={{textAlign: 'center'}}>
-          <h1>DBEnvy UI</h1>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div style={{
+        textAlign: 'center'
+      }}>
+        <h1>DBEnvy UI</h1>
+      </div>
+    );
+  }
 }
