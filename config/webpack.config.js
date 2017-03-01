a035fc73b3e92f037d0ed8093d552dcb5e222cbf
Zhao Yi <zhaoyi0113@gmail.com>
2017-03-01 12:15:44 +1100

    fix url-loader for woff

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -43,8 +43,8 @@ const config = (() => {
           ]
         },
         {
-          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "url-loader?limit=10000&mimetype=application/font-woff"
+          test: /\.(png|svg|woff|eot|ttf|woff2)(\?.*$|$)/,
+          loader: "url-loader?limit=100000&mimetype=application/font-woff"
         },
         {test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"},
       ]
