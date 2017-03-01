fb545591a0f0fd800832064de03a5f68c12d07ae
Michael Harrison <mike@southbanksoftware.com>
2017-03-01 12:35:56 +1100

    Removed static Blueprint CSS from folder. Added Codemirror dependancies. Added Codemirror example.

---
+++ src/components/codemirror-demo.jsx
@@ -0,0 +1,30 @@
+let React = require('react');
+let CodeMirror = require('react-codemirror');
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/markdown/markdown');
+
+export const Codemirror = React.createClass({
+  getInitialState () {
+    return {code: '// Code'};
+  },
+  updateCode (newCode) {
+    this.setState({code: newCode});
+  },
+  render () {
+    let options = {
+      mode: 'text/javascript',
+      matchBrackets: true,
+      json: true,
+      jsonld: true,
+      smartIndent: true,
+      theme: 'ambiance',
+      typescript: true,
+      lineNumbers: true
+    };
+    return (<CodeMirror
+      value={this.state.code}
+      onChange={this.updateCode}
+      options={options}/>);
+  }
+});
