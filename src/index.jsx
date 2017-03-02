f9bac93eabb5db5b0bbff5d67ed050ef9bdbfdf5
Zhao Yi <zhaoyi0113@gmail.com>
2017-03-03 10:45:24 +1100

    add strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 
+import { useStrict } from 'mobx';
+
+useStrict(true);
+
 const render = (Component) => ReactDOM.render(
   <Component/>, document.getElementById('root'));
 
