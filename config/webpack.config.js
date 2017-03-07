9dcf6131ccac13c07df538aa6194b9027fe6b473
Guan Gui <root@guiguan.net>
2017-03-07 18:36:50 +1100

    Added basic resizable layout and drawer

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,55 +1,16 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/**
+ * @Author: guiguan
+ * @Date:   2017-03-06T16:22:54+11:00
+ * @Last modified by:   guiguan
+ * @Last modified time: 2017-03-07T16:57:35+11:00
+ */
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const config = (() => {
-
-  return {
-    entry: [
-      "./src/index.jsx"
-    ],
-    output: {
-      path: path.join(__dirname, "../dist/"),
-      publicPath: "/",
-      filename: "app.[hash].js"
-    },
-    devtool: "source-map",
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader'],
-        },
-        {
-          test: /\.scss|css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: ["css-loader", 'sass-loader'],
-            publicPath: "styles/bundle.css"
-          })
-        },
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          use: [
-            "file-loader?hash=sha512&digest=hex&name=assets/[hash].[ext]",
-            "image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false"
-          ]
-        },
-        {
-          test: /\.(png|svg|woff|eot|ttf|woff2)(\?.*$|$)/,
-          loader: "url-loader?limit=100000&mimetype=application/font-woff"
-        },
-        {test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?name=images/[hash].[ext]"},
-      ]
-    },
-    plugins: getPlugins()
-  };
-});
-
 const getPlugins = () => {
-
   return [
     new webpack.DefinePlugin({
       'process.env': {
@@ -58,15 +19,60 @@ const getPlugins = () => {
     }),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({hash: false, template: "./src/index.html"}),
+    new HtmlWebpackPlugin({hash: false, template: './src/index.html'}),
     new ExtractTextPlugin({
-      filename: "styles/bundle.css",
+      filename: 'styles/bundle.css',
       disable: false,
       allChunks: true
     }),
   ];
+};
 
-}
-
+const config = (() => {
+  return {
+    entry: [
+      './src/index.jsx'
+    ],
+    output: {
+      path: path.join(__dirname, '../dist/'),
+      publicPath: '/',
+      filename: 'app.[hash].js'
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(jsx?)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.scss|css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader'],
+            publicPath: 'styles/bundle.css'
+          })
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          use: [
+            'file-loader?hash=sha512&digest=hex&name=assets/[hash].[ext]',
+            'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          ]
+        },
+        {
+          test: /\.(png|svg|woff|eot|ttf|woff2)(\?.*$|$)/,
+          loader: 'url-loader?limit=100000&mimetype=application/font-woff'
+        },
+        {test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=images/[hash].[ext]'},
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: getPlugins()
+  };
+});
 
 module.exports = config();
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,102 @@
-import React from 'react';
+/**
+ * @Author: guiguan
+ * @Date:   2017-03-07T13:47:00+11:00
+ * @Last modified by:   guiguan
+ * @Last modified time: 2017-03-07T18:35:03+11:00
+ */
 
-import '../../node_modules/normalize.css/normalize.css';
-import '../../node_modules/@blueprintjs/core/dist/blueprint.css';
-import '../../node_modules/codemirror/lib/codemirror.css';
-import '../../node_modules/codemirror/theme/ambiance.css';
+import React from 'react';
+import SplitPane from 'react-split-pane';
+import Drawer from 'react-motion-drawer';
+import { Button } from '@blueprintjs/core';
+
+import 'normalize.css/normalize.css';
+import '@blueprintjs/core/dist/blueprint.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/ambiance.css';
 import '../styles/global.scss';
-import {BlueprintDemo} from './blue-print-demo.jsx';
-import {NavbarDemo} from './navbar-demo.jsx';
-import {Codemirror} from './codemirror-demo.jsx';
-import TabDemo from './tab-demo.jsx';
+import './App.scss';
+
+// import {BlueprintDemo} from './blue-print-demo.jsx';
+// import {NavbarDemo} from './navbar-demo.jsx';
+// import {Codemirror} from './codemirror-demo.jsx';
+// import TabDemo from './tab-demo.jsx';
+//
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <div style={{
+//           textAlign: 'center'
+//         }}>
+//           <h1>DBEnvy UI Demo</h1>
+//           <NavbarDemo/>
+//         </div>
+//         <div className="middleRow">
+//           <div className="sidePanel">
+//             <h1>Side Panel.</h1>
+//           </div>
+//           <div className="editorPanel">
+//             <NavbarDemo />
+//             <Codemirror />
+//             <TabDemo/>
+//           </div>
+//           <div className="outputPanel">
+//             <h1>
+//               Output
+//             </h1>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default class App extends React.Component {
+  state = {
+    drawerOpen: open,
+  };
+
   render() {
+    const { drawerOpen } = this.state;
+
     return (
       <div>
-        <div style={{
-          textAlign: 'center'
-        }}>
-          <h1>DBEnvy UI Demo</h1>
-          <NavbarDemo/>
-        </div>
-        <div className="middleRow">
-          <div className="sidePanel">
-            <h1>Side Panel.</h1>
+        <Drawer
+          open={drawerOpen}
+          width="36%"
+          handleWidth={0}
+          onChange={open => this.setState({ drawerOpen: open })}
+        >
+          <div
+            style={{
+              backgroundColor: 'white',
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              flex: '1 1 0%',
+            }}
+          >
+            <p>Please close me!!!</p>
           </div>
-          <div className="editorPanel">
-            <NavbarDemo />
-            <Codemirror />
-            <TabDemo/>
-          </div>
-          <div className="outputPanel">
-            <h1>
-              Output
-            </h1>
-          </div>
-        </div>
+        </Drawer>
+        <SplitPane split="vertical" defaultSize="30%">
+          <SplitPane split="horizontal" defaultSize="50%">
+            <div>
+              <Button
+                iconName="pt-icon-menu-closed"
+                onClick={() => {
+                  this.setState({ drawerOpen: true });
+                }}
+              />
+            </div>
+            <div />
+          </SplitPane>
+          <SplitPane split="horizontal" defaultSize="70%">
+            <div />
+            <div />
+          </SplitPane>
+        </SplitPane>
       </div>
     );
   }
