f3bed74070b9ba388e3e33adee7a01e2f3ea66f5
Yi Zhao <zhaoyi0113@gmail.com>
2017-03-07 17:03:12 +1100

    fix eslint for blue print demo.jsx

--- src/components/blue-print-demo.jsx
+++ src/components/blue-print-demo.jsx
@@ -1,29 +1,27 @@
-import {Menu, MenuItem, MenuDivider, Popover, Button, Position} from "@blueprintjs/core";
-import React from "react";
-import {featherClient} from "../feathers/index";
+import {Menu, MenuItem, MenuDivider, Popover, Button, Position} from '@blueprintjs/core';
+import React from 'react';
+import {featherClient} from '../feathers/index';
 
 export const BlueprintDemo = () => {
-
   const menu = (
     <Menu className="pt-dark">
       <MenuItem text="New" onClick={() => {
         featherClient().createConnection('mongodb://dbenvy:DBEnvy2016@ec2-13-54-17-227.ap-southeast-2.compute.amazonaws.com')
-          .then(r => {
+          .then((r) => {
             console.log('response', r);
           })
-          .catch(err => {
+          .catch((err) => {
             console.err(err);
           });
-      }}/>
-      <MenuItem text="Run..."/>
-      <MenuItem text="Save"/>
+      }} />
+      <MenuItem text="Run..." />
+      <MenuItem text="Save" />
       <MenuDivider />
-      <MenuItem text="Settings..."/>
+      <MenuItem text="Settings..." />
     </Menu>
   );
 
-  return <Popover content={menu} position={Position.BOTTOM_RIGHT}>
-    <Button text="Actions"/>
-  </Popover>
-
-}
+  return (<Popover content={menu} position={Position.BOTTOM_RIGHT}>
+    <Button text="Actions" />
+  </Popover>);
+};
