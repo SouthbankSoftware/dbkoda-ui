38ac2b39b98bea09361b5c91a1e4a2350fe26e84
Guan Gui <root@guiguan.net>
2017-03-08 14:54:12 +1100

    Refactored to consistent with plural directory naming standard

---
+++ src/stores/global.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: guiguan
+ * @Date:   2017-03-07T18:37:59+11:00
+ * @Last modified by:   guiguan
+ * @Last modified time: 2017-03-08T14:53:29+11:00
+ */
+
+import {observable, action} from 'mobx';
