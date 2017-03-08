5db25d979d75d71854e6efc9fbfc3daf1d18791c
Michael Harrison <mike@southbanksoftware.com>
2017-03-08 14:00:32 +1100

    Implemented Loading Wheel for New Editor.

--- src/components/EditorPanel/Toolbar.jsx
+++ src/components/EditorPanel/Toolbar.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import {Intent} from '@blueprintjs/core';
+import {observer, observable} from 'mobx-react';
+import {Button, Intent} from '@blueprintjs/core';
 import {NewToaster} from '../common/Toaster.jsx';
 import {featherClient} from '../../helper/feathers';
 
-export default class Toolbar extends React.Component {
+/* eslint-disable react/sort-comp */
 
+@observer
+export default class Toolbar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      newConnectionLoading: false,
+      id: 0,
+      shellId: 0
+    };
 
-    this.addEditor = this.addEditor.bind(this);
+    this.addEditor = this
+      .addEditor
+      .bind(this);
     this.removeEditor = this
       .removeEditor
       .bind(this);
@@ -28,17 +37,27 @@ export default class Toolbar extends React.Component {
   // Linting disabled for this line.
   addEditor() { // eslint-disable-line class-methods-use-this
     try {
-      featherClient().service('/mongo-connection').create({}, {
-        query: {
-          url: 'mongodb://ec2-13-54-17-227.ap-southeast-2.compute.amazonaws.com',
-        }
-      }).then((res) => {
-        console.log('get response', res);
-        NewToaster.show({message: 'Connection Success!', intent: Intent.SUCCESS, iconName: 'pt-icon-thumbs-up'});
-      }).catch((err) => {
-        console.log('connection failed ', err.message);
-        NewToaster.show({message: err.message, intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
-      });
+      this.setState({newConnectionLoading: true});
+      featherClient()
+        .service('/mongo-connection')
+        .create({}, {
+          query: {
+            url: 'mongodb://ec2-13-54-17-227.ap-southeast-2.compute.amazonaws.com'
+          }
+        })
+        .then((res) => {
+          console.log('get response', res);
+          this.state.id = res.id;
+          this.state.shellId = res.shellId;
+          console.log(this.state.id, ' + ', this.state.shellId);
+          NewToaster.show({message: 'Connection Success!', intent: Intent.SUCCESS, iconName: 'pt-icon-thumbs-up'});
+          this.setState({newConnectionLoading: false});
+        })
+        .catch((err) => {
+          console.log('connection failed ', err.message);
+          NewToaster.show({message: err.message, intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
+          this.setState({newConnectionLoading: false});
+        });
     } catch (err) {
       NewToaster.show({message: 'Sorry, not yet implemented!', intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
     }
@@ -61,7 +80,9 @@ export default class Toolbar extends React.Component {
   }
   executeAll() {
     NewToaster.show({message: 'Sorry, not yet implemented!', intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
-    this.props.executeAll(); // eslint-disable-line react/prop-types
+    this
+      .props
+      .executeAll(); // eslint-disable-line react/prop-types
   }
 
   // Placeholder - Linting disabled for this line.
@@ -78,32 +99,33 @@ export default class Toolbar extends React.Component {
       <nav className="pt-navbar pt-dark editorToolBar">
         <div className="pt-navbar-group pt-align-left">
           <div className="pt-button-group">
-            <button
+            <Button
               className="pt-button pt-icon-add pt-intent-primary"
+              loading={this.state.newConnectionLoading}
               onClick={this.addEditor} />
-            <button
+            <Button
               className="pt-button pt-icon-delete pt-intent-primary"
               onClick={this.removeEditor} />
           </div>
           <span className="pt-navbar-divider" />
-          <button
+          <Button
             className="pt-button pt-icon-document-open pt-intent-primary"
             onClick={this.openFile} />
-          <button
+          <Button
             className="pt-button pt-icon-floppy-disk pt-intent-primary"
             onClick={this.saveFile} />
           <span className="pt-navbar-divider" />
           <div className="pt-button-group">
-            <button
+            <Button
               className="pt-button pt-icon-chevron-right pt-intent-primary"
               onClick={this.executeLine} />
-            <button
+            <Button
               className="pt-button pt-icon-double-chevron-right pt-intent-primary"
               onClick={this.executeAll} />
-            <button
+            <Button
               className="pt-button pt-icon-help pt-intent-primary"
               onClick={this.explainPlan} />
-            <button
+            <Button
               className="pt-button pt-icon-stop pt-intent-warning"
               onClick={this.stopExecution} />
           </div>
