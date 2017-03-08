660b0dd9765ee8a60c23cd4d46c4cf02bcdbc860
Guan Gui <root@guiguan.net>
2017-03-08 17:13:21 +1100

    Added profiles, editors and layout in store

--- src/stores/global.js
+++ src/stores/global.js
@@ -2,10 +2,12 @@
  * @Author: guiguan
  * @Date:   2017-03-07T18:37:59+11:00
  * @Last modified by:   guiguan
- * @Last modified time: 2017-03-08T14:53:29+11:00
+ * @Last modified time: 2017-03-08T17:12:40+11:00
  */
-import {observable, action} from 'mobx';
+import {observable} from 'mobx';
 
 export default class Store {
-
+  @observable profiles = observable.map();
+  @observable editors = observable.map();
+  @observable layout = {};
 }
