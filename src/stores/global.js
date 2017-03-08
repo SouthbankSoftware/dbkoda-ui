ea760935fe7e5a86dc252fcaab776ed5c395460d
Guan Gui <root@guiguan.net>
2017-03-08 19:37:46 +1100

    Connected layout with mobx store

--- src/stores/global.js
+++ src/stores/global.js
@@ -1,8 +1,8 @@
 /**
  * @Author: guiguan
  * @Date:   2017-03-07T18:37:59+11:00
-* @Last modified by:   wahaj
-* @Last modified time: 2017-03-08T17:22:36+11:00
+ * @Last modified by:   guiguan
+ * @Last modified time: 2017-03-08T19:18:30+11:00
  */
 
 import { observable, computed } from 'mobx';
@@ -11,7 +11,12 @@ import TempTopology from './TempTopology.js';
 export default class Store {
   @observable profiles = observable.map();
   @observable editors = observable.map();
-  @observable layout = {};
+  @observable layout = {
+    drawerOpen: false,
+    overallSplitPos: '30%',
+    leftSplitPos: '50%',
+    rightSplitPos: '70%',
+  };
 
   @computed get topology() {
     // this.getTopologyFromProfile()
