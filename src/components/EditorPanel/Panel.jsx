746103f56881a4a29390dc221bd6834c8ab08f5d
Michael Harrison <mike@southbanksoftware.com>
2017-03-09 12:41:46 +1100

    Further linting.

--- src/components/EditorPanel/Panel.jsx
+++ src/components/EditorPanel/Panel.jsx
@@ -93,7 +93,7 @@ export default class Panel extends React.Component {
           selectedTabId={this.state.tabId}>
           <Tab2 id={0} title="Default" panel={<View ref="defaultEditor" />} /> {editors.map((tab) => {
             return (
-              <Tab2 id={tab[0]} title={tab[1]} panel={<View ref="defaultEditor" />}>
+              <Tab2 key={tab[0]} id={tab[0]} title={tab[1]} panel={<View ref="defaultEditor" />}>
                 <Button
                   className="pt-intent-primary pt-minimal"
                   onClick={() => this.closeTab(tab[0], tab[1])}>
