68b73106530bb32f7eef6114f2dfca4b805a7be0
Wahaj Shamim <wahaj@southbanksoftware.com>
2017-03-10 12:06:37 +1100

    Minor Refactoring

--- src/components/TreePanel/Panel.jsx
+++ src/components/TreePanel/Panel.jsx
@@ -1,20 +1,21 @@
 /**
 * @Author: Wahaj Shamim <wahaj>
-* @Date:   2017-03-08T15:47:09+11:00
+* @Date:   2017-03-07T11:38:53+11:00
 * @Email:  wahaj@southbanksoftware.com
 * @Last modified by:   wahaj
-* @Last modified time: 2017-03-08T17:24:08+11:00
+* @Last modified time: 2017-03-10T09:53:34+11:00
 */
 
 import React from 'react';
 import { inject, observer, PropTypes, Provider } from 'mobx-react';
 
 import TreeState from './model/TreeState.js';
-import TreePanel from './Panel.jsx';
+import TreeToolbar from './Toolbar.jsx';
+import TreeView from './View.jsx';
 
 @inject('store')
 @observer
-export default class Tree extends React.Component {
+export default class TreePanel extends React.Component {
   componentWillMount() {
     this.treeState.parseJson(this.props.store.topology);
     console.log(this.treeState);
@@ -22,11 +23,14 @@ export default class Tree extends React.Component {
   treeState = new TreeState();
   render() {
     return (
+      <Provider treeState={this.treeState}>
       <div>
-        <Provider treeState={this.treeState}>
-          <TreePanel />
-        </Provider>
+
+          <TreeToolbar />
+          <TreeView />
+
       </div>
+      </Provider>
     );
   }
 }
---
+++ src/components/TreePanel/index.js
@@ -0,0 +1,15 @@
+/**
+* @Author: Wahaj Shamim <wahaj>
+* @Date:   2017-03-10T09:25:19+11:00
+* @Email:  wahaj@southbanksoftware.com
+* @Last modified by:   wahaj
+* @Last modified time: 2017-03-10T09:25:57+11:00
+*/
+
+
+
+import Panel from './Panel.jsx';
+
+export default {
+  TreePanel: Panel
+};
