036ad6458a95d3207ff6699b7a3524ba8e2ac3c4
Yi Zhao <zhaoyi0113@gmail.com>
2017-03-10 11:28:25 +1100

    update fethers client unit test

--- src/helpers/feathers/index.test.js
+++ src/helpers/feathers/index.test.js
@@ -3,10 +3,23 @@ import {featherClient} from './index';
 
 describe('test feathers client', () => {
   it('should register a listener', () => {
-    featherClient().addOutputListener(1, 2, 'test');
-    const listeners = featherClient().getShellOutputListeners(1, 2);
-    assert.equal(listeners.length, 1);
-    assert.equal(listeners[0].listeners.length, 1);
-    assert.equal(listeners[0].listeners[0], 'test');
+    featherClient().addOutputListener(1, 2, 'test1');
+    let output = featherClient().getShellOutputListeners(1, 2);
+    assert.equal(output.length, 1);
+    assert.equal(output[0].listeners.length, 1);
+    assert.equal(output[0].listeners[0], 'test1');
+
+    featherClient().addOutputListener(1, 2, 'test2');
+    output = featherClient().getShellOutputListeners(1, 2);
+    assert.equal(output.length, 1);
+    assert.equal(output[0].listeners.length, 2);
+
+    featherClient().removeOutputListener(1, 2, 'test1');
+    output = featherClient().getShellOutputListeners(1, 2);
+    assert.equal(output.length, 1);
+    assert.equal(output[0].listeners.length, 1);
+    assert.equal(output[0].listeners[0], 'test2');
+
+
   });
 });
