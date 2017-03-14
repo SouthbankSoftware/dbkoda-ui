525ab5a258c9be2b47feacfa95e58b5798604593
Michael Harrison <mike@southbanksoftware.com>
2017-03-14 13:55:16 +1100

    Changed Show More button to disabled.

--- src/components/OutputPanel/Toolbar.jsx
+++ src/components/OutputPanel/Toolbar.jsx
@@ -10,14 +10,16 @@ import React from 'react';
 import {inject, observer} from 'mobx-react';
 import {action} from 'mobx';
 import {NewToaster} from '../common/Toaster';
-import {Intent} from '@blueprintjs/core';
+import {Intent, Button} from '@blueprintjs/core';
 
-@inject(allStores => ({ output: allStores.store.output }) )
+@inject(allStores => ({output: allStores.store.output}))
 @observer
 export default class Toolbar extends React.Component {
   constructor(props) {
     super(props);
-    this.renderShowMoreBtn = this.renderShowMoreBtn.bind(this);
+    this.state = {
+      canShowMore: true
+    };
   }
 
   @action.bound
@@ -27,31 +29,7 @@ export default class Toolbar extends React.Component {
 
   @action.bound
   showMore() {
-    NewToaster.show({
-      message: 'Sorry, not yet implemented!',
-      intent: Intent.DANGER,
-      iconName: 'pt-icon-thumbs-down'
-    });
-  }
-
-  renderShowMoreBtn() {
-    if (this.props.output.canShowMore) {
-      return (<button
-        className="pt-button showMoreBtn"
-        onClick={this.showMore}
-        title="Show More (Shortcut+Keys)">
-        Show More
-      </button>);
-    }
-    else {
-      return (<button
-        className="pt-button pt-disabled showMoreBtn"
-        onClick={this.showMore}
-        title="Show More (Shortcut+Keys)"
-        disabled="disabled">
-        Show More
-      </button>);
-    }
+    NewToaster.show({message: 'Sorry, not yet implemented!', intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
   }
 
   render() {
@@ -59,14 +37,19 @@ export default class Toolbar extends React.Component {
       <nav className="pt-navbar pt-dark .modifier outputToolbar">
         <div className="pt-navbar-group pt-align-left">
           <div className="pt-navbar-heading">Query Output {this.props.blah}</div>
-          <button
-            className="pt-button pt-icon-disable clearOutputBtn"
+          <Button
+            className="pt-button pt-icon-disable pt-intent-warning clearOutputBtn"
             onClick={this.clearOutput}
-            title="Clear Output (Shortcut+Keys)"
-          />
-        {this.renderShowMoreBtn()}
+            title="Clear Output (Shortcut+Keys)" />
+          <Button
+            className="pt-button showMoreBtn pt-intent-primary"
+            onClick={this.showMore}
+            disabled={this.state.canShowMore}
+            title="Show More (Shortcut+Keys)">
+            Show More
+          </Button>
         </div>
-        <div className="pt-navbar-group pt-right-align"></div>
+        <div className="pt-navbar-group pt-right-align" />
       </nav>
     );
   }
