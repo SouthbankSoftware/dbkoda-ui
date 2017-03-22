2e9418333a9abd7dedb00b29bcfc7562594dcb9c
Yi Zhao <zhaoyi0113@gmail.com>
2017-03-22 11:18:19 +1100

    add test connection on new profile panel

--- src/components/ConnectionPanel/ProfileForm.js
+++ src/components/ConnectionPanel/ProfileForm.js
@@ -1,19 +1,39 @@
+/**
+ * profile form classs
+ */
 import MobxReactForm from 'mobx-react-form';
+import validatorjs from 'validatorjs';
 
-export default class ProfileForm extends MobxReactForm {
+export class ProfileForm extends MobxReactForm {
 
   static mongoProtocol = 'mongodb://';
 
   onInit() {
     // this.$('hostRadio').observe({
-    //   key: 'value', // can be any field property
+    //   key: 'value',
     //   call: ({form, field, change}) => {
-    //     console.log('xxxx');
-    //   }, // set new value here
+    //     console.log('xxxx,', field, change);
+    //     form.$('urlRadio').set('value', change.oldValue);
+    //   },
+    // });
+    // this.$('urlRadio').observe({
+    //   key: 'value',
+    //   call: ({form, field, change}) => {
+    //     console.log('xxxx,', field, change);
+    //     form.$('hostRadio').set('value', change.oldValue);
+    //   },
     // });
   }
 
   onSuccess(form) {
+    this.connect({...this.createFormData(form), test: false});
+  }
+
+  onTest() {
+    this.testConnect({...this.createFormData(this), test: true});
+  }
+
+  createFormData(form) {
     const formValues = form.values();
     let connectionUrl;
     if (formValues.hostRadio) {
@@ -25,12 +45,13 @@ export default class ProfileForm extends MobxReactForm {
       const split = connectionUrl.split(ProfileForm.mongoProtocol);
       connectionUrl = ProfileForm.mongoProtocol + formValues.username + ':' + formValues.password + '@' + split[1];
     }
-
-    this.connect({url: connectionUrl, database: formValues.database, authorization: true, test: false, alias: formValues.alias});
-  }
-
-  onTest(form) {
-    console.log('click test', form);
+    return {
+      url: connectionUrl,
+      database: formValues.database,
+      authorization: true,
+      test: false,
+      alias: formValues.alias
+    };
   }
 
   onError(form) {
@@ -49,3 +70,59 @@ export default class ProfileForm extends MobxReactForm {
   // }
 
 }
+
+export const Form = {
+  fields: [{
+    name: 'alias',
+    label: 'Alias',
+    placeholder: 'Alias',
+    value: 'Connection - 1',
+    type: 'text',
+    rules: 'required|string',
+  }, {
+    name: 'hostRadio',
+    value: false,
+    label: 'Host',
+  }, {
+    name: 'host',
+    placeholder: 'Hostname',
+    type: 'text',
+    rules: 'string',
+  }, {
+    name: 'port',
+    type: 'number',
+    rules: 'numeric',
+    value: '27017',
+  }, {
+    name: 'urlRadio',
+    label: 'URL',
+    value: true,
+  }, {
+    name: 'url',
+    label: 'URL',
+    placeholder: 'mongodb://',
+    value: 'mongodb://ec2-13-54-17-227.ap-southeast-2.compute.amazonaws.com',
+  }, {
+    name: 'database',
+    label: 'Database',
+    value: 'test',
+  }, {
+    name: 'ssl',
+    label: 'SSL',
+  }, {
+    name: 'sha',
+    value: false,
+    label: 'SCRAM-SHA-1(username/password)',
+  }, {
+    name: 'username',
+    label: 'Username',
+    value: 'dbenvy',
+  }, {
+    name: 'password',
+    label: 'Password',
+    type: 'password',
+  }]
+};
+
+
+export default new ProfileForm({fields: Form.fields}, {plugins: {dvr: validatorjs}});
