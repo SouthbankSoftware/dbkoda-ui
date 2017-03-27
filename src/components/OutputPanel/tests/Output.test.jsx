4a35ffd9413af9c3e99d6d16aef875210721e568
Chris Trott <chris@southbanksoftware.com>
2017-03-27 13:11:18 +1100

    Added tests for OutputToolbar

--- src/components/OutputPanel/tests/Output.test.jsx
+++ src/components/OutputPanel/tests/Output.test.jsx
@@ -3,60 +3,81 @@
 * @Date:   2017-03-10T10:55:54+11:00
 * @Email:  chris@southbanksoftware.com
  * @Last modified by:   chris
- * @Last modified time: 2017-03-16T17:32:47+11:00
+ * @Last modified time: 2017-03-27T13:06:21+11:00
 */
-
-import React from 'react';
-import { assert } from 'chai';
+import ReactDOM from 'react-dom';
+import { assert, expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import { useStrict } from 'mobx';
+import { Provider } from 'mobx-react';
+import renderer from 'react-test-renderer';
 import { OutputToolbar, OutputEditor, OutputPanel } from '../index.js';
 import Store from '~/stores/global';
+var jsdom = require("jsdom").jsdom;
+let React = require('react');
 
-describe('Output Panel', () => {
-  let app;
+describe('Output Toolbar', () => {
+  let document;
+  let window;
+  let store;
+  let OutputToolbarWrapper = function OutputToolbarWrapper(props) {
+    console.log(props.store.outputs.get('Test').cannotShowMore);
+    return (<Provider store={props.store}><OutputToolbar title="Test" /></Provider>);
+  }
 
   beforeAll(() => {
-    useStrict(true);
-    const store = new Store();
-    store.output.output = "Some output";
-    app = shallow(<OutputPanel.wrappedComponent store={store} />);
+    document = jsdom('<div id="container"></div>');
+    window = document.defaultView;
+    store = new Store();
   });
 
-  test ('should exist', () => {
-    assert(app.find('.outputPanel').exists());
+  beforeEach(() => {
+    store.outputs.set('Test', {
+      id: 1,
+      title: 'Test',
+      output: '',
+      cannotShowMore: true,
+      showingMore: false
+    });
   });
 
-  test ('can be updated programmatically');
-  test ('cannot be updated by the user');
-  test ('should be clearable');
+  test('should have an enabled showMoreBtn when cannotShowMore is false', () => {
+    store.outputs.get('Test').cannotShowMore = false;
+    ReactDOM.render(<OutputToolbarWrapper store={store} />, document.getElementById('container'));
+    let paragraph = document.querySelector('.showMoreBtn');
+    assert(!document.querySelector('.showMoreBtn').hasAttribute('disabled'));
+  });
+
+  test('should have a disabled showMoreBtn when cannotShowMore is true', () => {
+    store.outputs.get('Test').cannotShowMore = true;
+    ReactDOM.render(<OutputToolbarWrapper store={store} />, document.getElementById('container'));
+    let paragraph = document.querySelector('.showMoreBtn');
+    assert(document.querySelector('.showMoreBtn').hasAttribute('disabled'));
+  });
+
+  test('should have a clearOutputBtn', () => {
+    ReactDOM.render(<OutputToolbarWrapper store={store} />, document.getElementById('container'));
+    assert(document.querySelector('.clearOutputBtn'));
+  });
+
+  test('should have a saveOutputBtn', () => {
+    ReactDOM.render(<OutputToolbarWrapper store={store} />, document.getElementById('container'));
+    assert(document.querySelector('.saveOutputBtn'));
+  });
+
+});
+
+describe('Output Panel', () => {
+  test('has default tab');
+  test('new tab rendered');
+  test('tab visible');
+  test('tab not visible');
 });
 
 describe('Output Editor', () => {
-  test ('updates contents to reflect output', () => {
-    const wrapper = mount(<OutputEditor.wrappedComponent
-      output={{output: "Test Output", cannotShowMore: true}} />);
-
-  });
-  test ('clears contents when output is cleared');
-});
-
-describe('Output Toolbar', () => {
-  test ('should have an enabled showMoreBtn when cannotShowMore is false', () => {
-    const wrapper = shallow(<OutputToolbar.wrappedComponent
-      store={{output:{ output:"", cannotShowMore:false }}} />);
-    assert(!wrapper.find('.showMoreBtn').prop('disabled'));
-  });
-
-  test ('should have a disabled showMoreBtn when cannotShowMore is true', () => {
-    const wrapper = shallow(<OutputToolbar.wrappedComponent
-      store={{output:{ output:"", cannotShowMore:true }}} />);
-    assert(wrapper.find('.showMoreBtn').prop('disabled'));
-  });
-
-  test ('should have a clearOutputBtn', () => {
-    const wrapper = shallow(<OutputToolbar.wrappedComponent
-      store={{output:{ output:"", cannotShowMore:false }}} />);
-    assert(wrapper.find('.clearOutputBtn'));
-  });
+  test('updates contents to reflect output');
+  test('clears contents when output is cleared');
+  test('cannot be updated by the user');
+  test('componentDidUpdate()');
+  test('outputAvailable()');
 });
