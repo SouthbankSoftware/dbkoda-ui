a250de2edba9e540a0d104cf6afa5d0c230159f4
Yi Zhao <zhaoyi0113@gmail.com>
2017-03-28 12:27:23 +1100

    update connection profile list

--- src/components/ProfileListPanel/Toolbar.jsx
+++ src/components/ProfileListPanel/Toolbar.jsx
@@ -1,18 +1,17 @@
 /**
-* @Author: Michael Harrison <mike>
-* @Date:   2017-03-15 13:34:55
-* @Email:  mike@southbanksoftware.com
+ * @Author: Michael Harrison <mike>
+ * @Date:   2017-03-15 13:34:55
+ * @Email:  mike@southbanksoftware.com
  * @Last modified by:   mike
  * @Last modified time: 2017-03-15 13:34:51
-*/
-
+ */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/sort-comp */
 import React from 'react';
-import {observer, inject} from 'mobx-react';
+import {inject, observer} from 'mobx-react';
 import {action, runInAction} from 'mobx';
 import autobind from 'autobind-decorator';
-import {AnchorButton, Intent, Position, Tooltip, Alert} from '@blueprintjs/core';
+import {Alert, AnchorButton, Intent, Position, Tooltip} from '@blueprintjs/core';
 import {NewToaster} from '#/common/Toaster';
 import {ProfileStatus} from '../common/Constants';
 import {featherClient} from '../../helpers/feathers';
@@ -35,22 +34,22 @@ export default class Toolbar extends React.Component {
   }
 
   // Placeholder - Linting disabled for this line.
- @action newProfile() { // eslint-disable-line class-methods-use-this
-   this.props.store.profileList.selectedProfile = null;
-   this.props.store.layout.drawerOpen = true;
+  @action newProfile() { // eslint-disable-line class-methods-use-this
+    this.props.store.profileList.selectedProfile = null;
+    this.props.store.layout.drawerOpen = true;
   }
 
   // Placeholder - Linting disabled for this line.
   @action.bound
   editProfile() { // eslint-disable-line class-methods-use-this
     const {selectedProfile} = this.props.store.profileList;
-    if(selectedProfile){
-      if(selectedProfile.status === ProfileStatus.OPEN){
+    if (selectedProfile) {
+      if (selectedProfile.status === ProfileStatus.OPEN) {
         NewToaster.show({message: 'Connection is not closed.', intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
-      }else {
+      } else {
         this.props.store.layout.drawerOpen = true;
       }
-    }else{
+    } else {
       NewToaster.show({message: 'No Profile Selected!', intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
     }
   }
@@ -64,33 +63,37 @@ export default class Toolbar extends React.Component {
   }
 
   @autobind
-  showRemoveConnectionAlert(){
+  showRemoveConnectionAlert() {
     this.setState({removeConnectionAlert: true});
   }
 
   @autobind
-  hideRemoveConnectionAlert(){
+  hideRemoveConnectionAlert() {
     this.setState({removeConnectionAlert: false});
   }
 
   @action.bound
-  closeProfile(){
+  closeProfile() {
     const {selectedProfile} = this.props.store.profileList;
-    if(selectedProfile){
+    const {profiles} = this.props.store;
+    if (selectedProfile) {
       console.log('close profile ', selectedProfile.id);
       this.setState({closingProfile: true});
       featherClient().service('/mongo-connection').remove(selectedProfile.id)
-        .then(v=>{
+        .then(v => {
           console.log('got close response ', v);
-          this.setState({closingProfile: false, closeConnectionAlert:false});
-          runInAction(()=>selectedProfile.status = ProfileStatus.CLOSED);
+          this.setState({closingProfile: false, closeConnectionAlert: false});
+          runInAction(() => {
+            selectedProfile.status = ProfileStatus.CLOSED;
+            profiles.set(selectedProfile.id, selectedProfile);
+          });
           NewToaster.show({message: 'Connection Closed', intent: Intent.SUCCESS, iconName: 'pt-icon-thumbs-up'});
           Broker.emit(EventType.PROFILE_CLOSED, selectedProfile.id);
         })
-        .catch(err=>{
+        .catch(err => {
           console.log('error:', err);
           NewToaster.show({message: err.message, intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
-          this.setState({closingProfile: false, closeConnectionAlert:false});
+          this.setState({closingProfile: false, closeConnectionAlert: false});
         })
     } else {
       NewToaster.show({message: 'No Profile Selected!', intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
@@ -98,7 +101,7 @@ export default class Toolbar extends React.Component {
   }
 
   @autobind
-  hideCloseConnectionAlert(){
+  hideCloseConnectionAlert() {
     this.setState({closeConnectionAlert: false});
   }
 
@@ -113,12 +116,14 @@ export default class Toolbar extends React.Component {
       <nav className="pt-navbar profileListToolbar">
         <div className="pt-navbar-group pt-align-left">
           <div className="pt-button-group">
-            <Alert className="pt-dark" intent={Intent.PRIMARY} isOpen={this.state.closeConnectionAlert} confirmButtonText="Close Connection"
+            <Alert className="pt-dark" intent={Intent.PRIMARY} isOpen={this.state.closeConnectionAlert}
+                   confirmButtonText="Close Connection"
                    cancelButtonText="Cancel" onConfirm={this.closeProfile}
                    onCancel={this.hideCloseConnectionAlert}>
               <p>Are you sure you want to close this connection?</p>
             </Alert>
-            <Alert className="pt-dark" intent={Intent.PRIMARY} isOpen={this.state.removeConnectionAlert} confirmButtonText="Remove Connection"
+            <Alert className="pt-dark" intent={Intent.PRIMARY} isOpen={this.state.removeConnectionAlert}
+                   confirmButtonText="Remove Connection"
                    cancelButtonText="Cancel" onConfirm={this.removeProfile}
                    onCancel={this.hideRemoveConnectionAlert}>
               <p>Are you sure you want to remove this connection?</p>
@@ -131,8 +136,7 @@ export default class Toolbar extends React.Component {
               position={Position.BOTTOM_LEFT}>
               <AnchorButton
                 className="pt-button pt-icon-add pt-intent-primary newProfileButton"
-                loading={this.props.store.profileList.creatingNewProfile}
-                onClick={this.newProfile} />
+                onClick={this.newProfile}/>
             </Tooltip>
             <Tooltip
               intent={Intent.PRIMARY}
@@ -143,7 +147,7 @@ export default class Toolbar extends React.Component {
               <AnchorButton
                 className="pt-button pt-icon-edit pt-intent-primary editProfileButton"
                 onClick={this.editProfile}
-                disabled={!selectedProfile || selectedProfile.status === ProfileStatus.OPEN } />
+                disabled={!selectedProfile || selectedProfile.status === ProfileStatus.OPEN }/>
             </Tooltip>
             <Tooltip intent={Intent.PRIMARY}
                      hoverOpenDelay={1000}
@@ -153,7 +157,7 @@ export default class Toolbar extends React.Component {
               <AnchorButton className="pt-icon-remove pt-intent-danger closeProfileButton"
                             loading={this.state.closingProfile}
                             disabled={!selectedProfile
-                              || selectedProfile.status === ProfileStatus.CLOSED}
+                            || selectedProfile.status === ProfileStatus.CLOSED}
                             onClick={this.showCloseConnectionAlert}/>
             </Tooltip>
             <Tooltip
@@ -165,11 +169,11 @@ export default class Toolbar extends React.Component {
               <AnchorButton
                 className="pt-button pt-icon-cross pt-intent-danger removeProfileButton"
                 onClick={this.showRemoveConnectionAlert}
-                disabled = {!this.props.store.profileList.selectedProfile
-                || this.props.store.profileList.selectedProfile.status === ProfileStatus.OPEN} />
+                disabled={!this.props.store.profileList.selectedProfile
+                || this.props.store.profileList.selectedProfile.status === ProfileStatus.OPEN}/>
             </Tooltip>
           </div>
-          <span className="pt-navbar-divider" />
+          <span className="pt-navbar-divider"/>
           <Tooltip
             intent={Intent.NONE}
             hoverOpenDelay={1000}
@@ -177,13 +181,13 @@ export default class Toolbar extends React.Component {
             tooltipClassName="pt-dark"
             position={Position.BOTTOM}>
             <div className="pt-input-group .modifier">
-              <span className="pt-icon pt-icon-search" />
+              <span className="pt-icon pt-icon-search"/>
               <input
                 className="pt-input"
                 type="search"
                 placeholder="Filter Profiles..."
                 dir="auto"
-                onChange={this.onFilterChanged} />
+                onChange={this.onFilterChanged}/>
             </div>
           </Tooltip>
         </div>
