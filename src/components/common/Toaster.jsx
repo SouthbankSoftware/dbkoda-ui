0efef7db27aa6dc6fa0b228266591f9b8b896ece
Michael Harrison <mike@southbanksoftware.com>
2017-03-28 16:24:55 +1100

    Linting ESDoc

--- src/components/common/Toaster.jsx
+++ src/components/common/Toaster.jsx
@@ -1,4 +1,9 @@
-// toaster.ts
+/**
+ * @Author: mike
+ * @Date:   2017-02-15 16:13:50
+ * @Last modified by:   mike
+ * @Last modified time: 2017-03-28 16:22:02
+ */
 import {Position, Toaster} from '@blueprintjs/core';
 
 export const NewToaster = Toaster.create({className: 'my-toaster', position: Position.TOP_RIGHT});
