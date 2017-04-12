36e3b10bc5353844307e86b5faee647cbc62bf32
michaeljharrison <mike@southbanksoftware.com>
2017-04-12 15:01:17 +1000

    Updated Sample Attributes (Still WIP)

--- src/components/TreePanel/model/TreeState.js
+++ src/components/TreePanel/model/TreeState.js
@@ -7,7 +7,10 @@
 */
 
 import {observable, action} from 'mobx';
-
+import {featherClient} from '~/helpers/feathers';
+import {NewToaster} from '#/common/Toaster';
+import {Intent} from '@blueprintjs/core';
+import EventLogging from '#/common/logging/EventLogging';
 import TreeNode from './TreeNode.jsx';
 
 export default class TreeState {
@@ -21,6 +24,7 @@ export default class TreeState {
   resetTreeNode;
   @observable profileAlias = '';
   updateCallback;
+  updateCallback2;
   constructor() {
     this.treeNodes = [];
     this.filteredNodes = observable([]);
@@ -152,53 +156,82 @@ export default class TreeState {
    * @param {Object} nodeRightClicked - The Node that triggered this action.
    */
   sampleCollection(nodeRightClicked) {
-    const database = nodeRightClicked.refParent.text;
+    const db = nodeRightClicked.refParent.text;
     const queryFirst = 'use ' + database; // eslint-disable-line
-    const querySecond = 'db.' + nodeRightClicked.text + '.aggregate({$sample: {size: 100}})'; // eslint-disable-line
-    console.log(queryfirst, querySeconds);
-    // This will be sent to tree service and return a JSON object of the appropriate
-    // structure.
-    const sampleStructure = {
-      text: 'Collection Sample',
-      type: 'properties',
-      children: [
-        {
-          text: 'Name',
-          type: 'label'
-        }, {
-          text: 'Date of Birth',
-          type: 'numerical'
-        }, {
-          text: 'Address',
-          type: 'properties',
-          children: [
-            {
-              text: 'State',
-              type: 'label'
-            }, {
-              text: 'PostCode',
-              type: 'numerical'
-            }, {
-              text: 'Street Address',
-              type: 'numerical'
-            }
-          ]
+    const querySecond = 'db.' + nodeRightClicked.text + '.aggregate({$sample: {size: 20}})'; // eslint-disable-line
+    const collection = nodeRightClicked.text;
+    const profile = this.updateCallback2();
+
+    let sampleJSON;
+    const service = featherClient().service('/mongo-driver');
+    service.timeout = 30000;
+    service
+      .get({
+        id: profile.id,
+        database: db,
+        type: 'sample-collection'
+      }, {})
+      .then((res) => {
+        sampleJSON = this.parseSampleData(res);
+        if (true) {
+          sampleJSON = {
+            text: 'Collection Sample',
+            type: 'properties',
+            children: [
+              {
+                text: 'Name',
+                type: 'label'
+              }, {
+                text: 'Date of Birth',
+                type: 'numerical'
+              }, {
+                text: 'Address',
+                type: 'properties',
+                children: [
+                  {
+                    text: 'State',
+                    type: 'label'
+                  }, {
+                    text: 'PostCode',
+                    type: 'numerical'
+                  }, {
+                    text: 'Street Address',
+                    type: 'numerical'
+                  }
+                ]
+              }
+            ]
+          };
         }
-      ]
-    };
-    if (!nodeRightClicked.allChildNodes) {
-      nodeRightClicked.allChildNodes = new Map();
-    }
-    const child = new TreeNode(sampleStructure, nodeRightClicked);
-    console.log('Tree Created Child: ', child);
-    nodeRightClicked.isExpanded = true;
-    child.isExpanded = true;
-    nodeRightClicked.setFilter(this.filter);
-    this.updateCallback();
-    nodeRightClicked.isExpanded = true;
-    this.updateCallback();
-    // Force re-render of tree.
+        if (!nodeRightClicked.allChildNodes) {
+          nodeRightClicked.allChildNodes = new Map();
+        }
+        const child = new TreeNode(sampleJSON, nodeRightClicked);
+        console.log('Tree Created Child: ', child);
+        nodeRightClicked.isExpanded = true;
+        child.isExpanded = true;
+        nodeRightClicked.setFilter(this.filter);
+        this.updateCallback();
+        nodeRightClicked.isExpanded = true;
+        this.updateCallback();
+        // Force re-render of tree.
+      })
+      .catch((err) => {
+        NewToaster.show({message: err.message, intent: Intent.DANGER, iconName: 'pt-icon-thumbs-down'});
+      });
   }
+
+  /**
+   * Parse sample data from shell and create a tree sample structure.
+   * @param {Object[]} queryResult - An array of JSON objects from the collection.
+   * @return {Object} - The resulting tree structure.
+   */
+  parseSampleData(queryResult) {
+    console.log('Sample Result: ', queryResult);
+    // @TODO -> Parse 100 JSON rows and return common keys.
+    return queryResult;
+  }
+
   /**
    * Returns the tree nodes to bind to react tree component
    * @return {[type]} [description]
