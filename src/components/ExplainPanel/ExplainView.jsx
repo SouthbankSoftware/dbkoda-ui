d87663853ff37f9177ecdead07a675aece49d453
Yi Zhao <zhaoyi0113@gmail.com>
2017-04-13 17:05:13 +1000

    add explain view for stages

--- src/components/ExplainPanel/ExplainView.jsx
+++ src/components/ExplainPanel/ExplainView.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './style.scss';
+import {Types} from './Types';
+import {Button, Popover, PopoverInteractionKind, Position} from '@blueprintjs/core';
+import JSONTree from 'react-json-tree';
 
-
+/**
+ * show explain command information
+ */
 const ExplainCommandInfo = ({explain, command}) => {
   console.log('command info ', explain, command);
   return (
@@ -20,26 +25,121 @@ const ExplainCommandInfo = ({explain, command}) => {
   );
 };
 
+export const findExecutionStage = (stages, stage) => {
+  let currentStage = stages;
+  while (currentStage) {
+    if (currentStage.stage === stage) {
+      return currentStage;
+    }
+    currentStage = currentStage.inputStage;
+  }
+};
+
+export const getExecutionStages = (executionStages) => {
+  const stages = [];
+  if (executionStages) {
+    let currentStage = executionStages;
+    while (currentStage) {
+      stages.push(currentStage);
+      currentStage = currentStage.inputStage;
+    }
+  } else {
+    stages.push(explain.queryPlanner.winningPlan);
+  }
+  return stages;
+};
+
+/**
+ * show winning plain
+ */
+const WinningPlain = ({explain, type}) => {
+  let stages;
+  switch (type) {
+    case Types.ALL_PLANS_EXECUTION:
+    case Types.EXECUTION_STATS:
+      stages = getExecutionStages(explain.executionStats.executionStages).reverse();
+      return (<div style={{marginLeft: 10}}>
+        {
+          stages.map((stage, i) => {
+            console.log('render stage ', stage, JSON.stringify(stage));
+            return (<div key={i} style={{display: 'flex'}}>
+              <Popover
+                content={<JSONTree data={stage}/>}
+                interactionKind={PopoverInteractionKind.CLICK}
+                position={Position.TOP_RIGHT}
+                useSmartPositioning={false}
+                popoverClassName="explain-stage-detail-popup"
+              >
+                <Button className="explain-stage-button">{stage.stage}</Button>
+              </Popover>
+              <ul className="explain-output-column-label">
+                <li className="explain-output-list-label">nReturned:</li>
+                <li className="explain-output-list-label">Execution Time:</li>
+                <li
+                  className="explain-output-list-label">{stage.stage === 'IXSCAN' ? 'Key Examined:' : 'Doc Examined:'}</li>
+              </ul>
+              <ul className="explain-output-column-label">
+                <li className="explain-output-list-content">{stage.nReturned}</li>
+                <li className="explain-output-list-content">{stage.executionTimeMillisEstimate}</li>
+                <li
+                  className="explain-output-list-content">{stage.stage === 'IXSCAN' ? stage.keysExamined : stage.docsExamined}</li>
+              </ul>
+            </div>);
+          })
+        }
+      </div>);
+    default:
+      stages = getExecutionStages(explain.queryPlanner.winningPlan).reverse();
+      return (<div style={{marginLeft: 10}}>
+        <Popover
+          content={<JSONTree data={stages[0]}/>}
+          interactionKind={PopoverInteractionKind.CLICK}
+          popoverClassName="explain-stage-detail-popup"
+          position={Position.TOP_RIGHT}
+          useSmartPositioning={false}
+        >
+          <Button className="explain-stage-button">{stages[0].stage}</Button>
+        </Popover>
+      </div>);
+  }
+};
+
+const GlobalStatistics = ({explains, type}) => {
+  switch (type) {
+    case Types.ALL_PLANS_EXECUTION:
+    case Types.EXECUTION_STATS:
+      return (<div>
+        <div style={{margin: 10}}>Global Statistics</div>
+        <ul className="explain-output-column-label">
+          <li className="explain-output-list-label">plannerVersion:</li>
+          <li className="explain-output-list-label">totalDocsExamined:</li>
+          <li className="explain-output-list-label">totalKeysExamined:</li>
+          <li className="explain-output-list-label">nReturned:</li>
+        </ul>
+        <ul className="explain-output-column-label">
+          <li className="explain-output-list-content">{explains.queryPlanner.plannerVersion}</li>
+          <li className="explain-output-list-content">{explains.executionStats.totalDocsExamined}</li>
+          <li className="explain-output-list-content">{explains.executionStats.totalKeysExamined}</li>
+          <li className="explain-output-list-content">{explains.executionStats.nReturned}</li>
+        </ul>
+      </div>);
+    default:
+      return (<div>
+
+      </div>);
+  }
+}
+
 const ExplainView = ({explains}) => {
   if (!explains.output) {
     return null;
   }
-  console.log('explains:', explains);
   const output = JSON.parse(explains.output);
-  console.log('output:', output);
   return (<div className="explain-view-panel">
-    <ExplainCommandInfo explain={output} command={explains.command} />
-    <table className="pt-table">
-      <tr>
-        <td>Document Returned:</td>
-      </tr>
-      <tr>
-        <td>Index Keys Examined:</td>
-      </tr>
-      <tr>
-        <td>Technologies:</td>
-      </tr>
-    </table>
+    <ExplainCommandInfo explain={output} command={explains.command}/>
+    <div style={{margin: 10}}>Winning Plan</div>
+    <WinningPlain explain={output} type={explains.type}/>
+    <GlobalStatistics explains={output} type={explains.type}/>
   </div>);
 };
 
---
+++ src/components/ExplainPanel/tests/ExplainView.test.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import {useStrict} from 'mobx';
+import chai, {expect, assert} from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import {getExecutionStages} from '../ExplainView';
+
+chai.use(chaiEnzyme());
+
+describe('test explain view', () => {
+  it('test parse stages output', () => {
+    const explainOutput = {
+      'queryPlanner': {
+        'plannerVersion': 1,
+        'namespace': 'm102.restaurants',
+        'indexFilterSet': false,
+        'parsedQuery': {'address.zipcode': {'$eq': '18882'}},
+        'winningPlan': {
+          'stage': 'FETCH',
+          'inputStage': {
+            'stage': 'IXSCAN',
+            'keyPattern': {'address.zipcode': 1, 'cuisine': 1, 'stars': 1},
+            'indexName': 'address.zipcode_1_cuisine_1_stars_1',
+            'isMultiKey': false,
+            'multiKeyPaths': {'address.zipcode': [], 'cuisine': [], 'stars': []},
+            'isUnique': false,
+            'isSparse': false,
+            'isPartial': false,
+            'indexVersion': 2,
+            'direction': 'forward',
+            'indexBounds': {
+              'address.zipcode': ['["18882", "18882"]'],
+              'cuisine': ['[MinKey, MaxKey]'],
+              'stars': ['[MinKey, MaxKey]']
+            }
+          }
+        },
+        'rejectedPlans': []
+      },
+      'executionStats': {
+        'executionSuccess': true,
+        'nReturned': 8,
+        'executionTimeMillis': 0,
+        'totalKeysExamined': 8,
+        'totalDocsExamined': 8,
+        'executionStages': {
+          'stage': 'FETCH',
+          'nReturned': 8,
+          'executionTimeMillisEstimate': 0,
+          'works': 9,
+          'advanced': 8,
+          'needTime': 0,
+          'needYield': 0,
+          'saveState': 0,
+          'restoreState': 0,
+          'isEOF': 1,
+          'invalidates': 0,
+          'docsExamined': 8,
+          'alreadyHasObj': 0,
+          'inputStage': {
+            'stage': 'IXSCAN',
+            'nReturned': 8,
+            'executionTimeMillisEstimate': 0,
+            'works': 9,
+            'advanced': 8,
+            'needTime': 0,
+            'needYield': 0,
+            'saveState': 0,
+            'restoreState': 0,
+            'isEOF': 1,
+            'invalidates': 0,
+            'keyPattern': {'address.zipcode': 1, 'cuisine': 1, 'stars': 1},
+            'indexName': 'address.zipcode_1_cuisine_1_stars_1',
+            'isMultiKey': false,
+            'multiKeyPaths': {'address.zipcode': [], 'cuisine': [], 'stars': []},
+            'isUnique': false,
+            'isSparse': false,
+            'isPartial': false,
+            'indexVersion': 2,
+            'direction': 'forward',
+            'indexBounds': {
+              'address.zipcode': ['["18882", "18882"]'],
+              'cuisine': ['[MinKey, MaxKey]'],
+              'stars': ['[MinKey, MaxKey]']
+            },
+            'keysExamined': 8,
+            'seeks': 1,
+            'dupsTested': 0,
+            'dupsDropped': 0,
+            'seenInvalidated': 0
+          }
+        },
+        'allPlansExecution': []
+      },
+      'serverInfo': {
+        'host': 'e8d1597fe5e4',
+        'port': 27017,
+        'version': '3.4.0',
+        'gitVersion': 'f4240c60f005be757399042dc12f6addbc3170c1'
+      },
+      'ok': 1
+    };
+
+    let stages = getExecutionStages(explainOutput.executionStats.executionStages);
+    assert.equal(stages.length, 2);
+    assert.equal(stages[0].stage, 'FETCH');
+    assert.equal(stages[1].stage, 'IXSCAN');
+
+    stages = getExecutionStages(explainOutput.queryPlanner.winningPlan);
+    assert.equal(stages.length, 2);
+    assert.equal(stages[0].stage, 'FETCH');
+    assert.equal(stages[1].stage, 'IXSCAN');
+  });
+
+  it('test plain explain output ', () => {
+    const output = JSON.parse('{"queryPlanner" : {"plannerVersion" : 1,"namespace" : "m102.restaurants","indexFilterSet" : false,"parsedQuery" : {},"winningPlan" : {"stage" : "COLLSCAN","direction" : "forward"},"rejectedPlans" : [ ]},"executionStats" : {"executionSuccess" : true,"nReturned" : 1000000,"executionTimeMillis" : 196,"totalKeysExamined" : 0,"totalDocsExamined" : 1000000,"executionStages" : {"stage" : "COLLSCAN","nReturned" : 1000000,"executionTimeMillisEstimate" : 74,"works" : 1000002,"advanced" : 1000000,"needTime" : 1,"needYield" : 0,"saveState" : 7816,"restoreState" : 7816,"isEOF" : 1,"invalidates" : 0,"direction" : "forward","docsExamined" : 1000000},"allPlansExecution" : [ ]},"serverInfo" : {"host" : "e8d1597fe5e4","port" : 27017,"version" : "3.4.0","gitVersion" : "f4240c60f005be757399042dc12f6addbc3170c1"},"ok" : 1}');
+    const stages = getExecutionStages(output.queryPlanner.winningPlan);
+    assert.equal(stages.length, 1);
+    assert.equal(stages[0].stage, 'COLLSCAN');
+  });
+
+
+});
