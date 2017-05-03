7c46ad51a708332070ac1a1c354cb885a595b887
Guy Harrison <guy.a.harrison@gmail.com>
2017-05-03 16:09:56 +1000

    Create standard index

---
+++ src/components/TreeActionPanel/Functions/CreateIndex.js
@@ -0,0 +1,23 @@
+/**
+ * @Author: Wahaj Shamim <wahaj>
+ * @Date:   2017-04-03T16:14:52+10:00
+ * @Email:  wahaj@southbanksoftware.com
+ * @Last modified by:   wahaj
+ * @Last modified time: 2017-05-03T12:12:33+10:00
+ */
+
+export const CreateIndex = {
+  executeCommand: null,
+  setExecuteFunction: (cbFuncExecute) => {
+    CreateIndex.executeCommand = cbFuncExecute;
+  },
+  // Prefill function for alter user
+  dbenvy_CreateIndexPreFill: (collectionName) => {
+    const data = {};
+    data.CollectionName = collectionName;
+    data.Unique = 'false';
+    data.Background = 'false';
+    data.Sparse = 'false';
+    return data;
+  }
+};
---
+++ src/components/TreeActionPanel/tests/createIndex.hbs.test.js
@@ -0,0 +1,61 @@
+/**
+*  Unit Test for ALter user template
+
+* @Author: Guy Harrison
+
+*/
+//
+// Unit test for AlterUser template
+//
+// TODO: Fix dependency on local mongo (use mlaunch?)
+const templateToBeTested = './src/components/TreeActionPanel/Templates/CreateIndex.hbs';
+const templateInput = require('./CreateIndex.hbs.input.json');
+const hbs = require('handlebars');
+const fs = require('fs');
+const sprintf = require('sprintf-js').sprintf;
+const common = require('./common.js');
+
+// Random collection for the test
+const randomCollectionName = 'collection' + Math.floor(Math.random() * 10000000);
+const randomIndexName = randomCollectionName + '_i';
+
+templateInput.CollectionName = randomCollectionName;
+templateInput.IndexName = randomIndexName;
+
+// Command to drop the user
+const setupCollectionCommands = [];
+setupCollectionCommands.push(sprintf('use test\n'));
+setupCollectionCommands.push(sprintf('db.%s.drop();\n', randomCollectionName));
+setupCollectionCommands.push(sprintf('db.%s.insertOne({a:1,b:1,c:{d:1,e:1}});\n', randomCollectionName));
+
+// Command that checks the user is OK
+const validateIndexCmd = sprintf('\ndb.%s.getIndexes().forEach(i=>{if (i.name==="%s") {print ("Found index "+i.name)' +
+    '}});',
+randomCollectionName, randomIndexName);
+const dropCollectionCmd = sprintf('\ndb.%s.drop();\n', randomCollectionName);
+
+// Run the test
+test('Create Index template', (done) => {
+  fs.readFile(templateToBeTested, (err, data) => {
+    if (!err) {
+      const templateSource = data.toString();
+      const compiledTemplate = hbs.compile(templateSource);
+      const createIndexCommands = compiledTemplate(templateInput);
+      let mongoCommands = '';
+      setupCollectionCommands.forEach((c) => {
+        mongoCommands += c;
+      });
+      mongoCommands += createIndexCommands;
+      mongoCommands += validateIndexCmd;
+      mongoCommands += dropCollectionCmd + '\nexit\n';
+      // console.log(mongoCommands);
+      const matchString = sprintf('Found index %s', randomIndexName);
+      common
+        .mongoOutput(mongoCommands)
+        .then((output) => {
+          expect(output).toEqual(expect.stringMatching(matchString));
+          done();
+        });
+    }
+  });
+});
