d06c058621a37c8c20c4095069c7c811d564206e
Wahaj Shamim <wahaj@southbanksoftware.com>
2017-05-09 16:44:18 +1000

    Minor Fixes for field definitions.

--- src/components/TreeActionPanel/Components/FormTableRow.jsx
+++ src/components/TreeActionPanel/Components/FormTableRow.jsx
@@ -3,7 +3,7 @@
  * @Date:   2017-04-18T13:51:12+10:00
  * @Email:  wahaj@southbanksoftware.com
  * @Last modified by:   wahaj
- * @Last modified time: 2017-05-09T11:07:14+10:00
+ * @Last modified time: 2017-05-09T16:43:23+10:00
  */
 
 import React from 'react';
@@ -12,6 +12,8 @@ import { observer } from 'mobx-react';
 import FieldControl from './FieldControls';
 import TextField from './TextField';
 import SelectField from './SelectField';
+import NumericField from './NumericField';
+import BooleanField from './BooleanField';
 
 export default observer(({ member }) => {
   const rowFields = [];
@@ -20,6 +22,10 @@ export default observer(({ member }) => {
       rowFields.push(<TextField key={member.id + col.key} field={col} showLabel={false} formGroup />);
     } else if (col.type == 'Select') {
       rowFields.push(<SelectField key={member.id + col.key} field={col} showLabel={false} formGroup />);
+    } else if (col.type == 'Numeric') {
+      rowFields.push(<NumericField key={member.id + col.key} field={col} showLabel={false} formGroup />);
+    } else if (col.type == 'Boolean') {
+      rowFields.push(<NumericField key={member.id + col.key} field={col} formGroup />);
     }
   });
 
