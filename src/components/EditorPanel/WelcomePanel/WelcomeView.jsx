87f5af22ef33152eb32713c4e0e24b81b4f68978
michaeljharrison <mike@southbanksoftware.com>
2017-05-16 15:05:37 +1000

    Started Revamping Welcome Page.

--- src/components/EditorPanel/WelcomePanel/WelcomeView.jsx
+++ src/components/EditorPanel/WelcomePanel/WelcomeView.jsx
@@ -9,9 +9,10 @@
 /* eslint-disable react/no-string-refs */
 import React from 'react';
 import {inject, observer} from 'mobx-react';
-import News from './News.jsx';
-import Info from './Info.jsx';
-
+import WelcomeMenu from './WelcomeMenu.jsx';
+import WelcomeContent from './WelcomeContent.jsx';
+import ChooseTheme from './ChooseTheme.jsx';
+import LearnShortcuts from './LearnShortcuts.jsx';
 
 /**
  * Panel for wrapping the Editor View and EditorToolbar.
@@ -19,20 +20,41 @@ import Info from './Info.jsx';
  */
 @inject('store')
 @observer
-export default class Panel extends React.Component {
+export default class WelcomeView extends React.Component {
   static propTypes = {};
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+  renderWelcomePage() {
+    switch (this.props.store.welcomePage.currentContent) {
+      case 'Welcome':
+        return <WelcomeContent />;
+      case 'Choose Theme':
+        return <ChooseTheme />;
+      case 'Keyboard Shortcuts':
+        return <LearnShortcuts />;
+      default:
+        return <div>ERROR</div>;
+    }
+  }
+
   render() {
     return (
-      <div className="welcomePanelWrapper">
-        <h1>Welcome to DBEnvy</h1>
-        <div className="welcomePanel">
-          <News />
-          <Info />
+      <div className="welcomePanelTabWrapper">
+        <div className="welcomePanelWrapper">
+          <h1>
+            Welcome To DBEnvy
+          </h1>
+          <div className="welcomeMiddleSection">
+            <div className="welcomeMenuWrapper">
+              <WelcomeMenu />
+            </div>
+            <div className="welcomeContentWrapper">
+              {this.renderWelcomePage()}
+            </div>
+          </div>
         </div>
       </div>
     );
