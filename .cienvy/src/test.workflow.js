1a02cd782fcd12989e68f51bd11cace9788e419a
Guan Gui <root@guiguan.net>
2017-05-19 12:00:28 +1000

    Improved `yarn test`
    1. `yarn test` will be used uniformly on local and ci
    2. `yarn test:dev` is convenient script for local dev which only runs test without setting/cleaning up required mongod
    3. `yarn mlaunch:init` setup mongod
    4. `yarn mlaunch:clean` destroy mongod and any directories created

--- .cienvy/src/test.workflow.js
+++ .cienvy/src/test.workflow.js
@@ -2,14 +2,9 @@
  * @Author: guiguan
  * @Date:   2017-05-15T18:35:28+10:00
  * @Last modified by:   guiguan
- * @Last modified time: 2017-05-17T00:13:26+10:00
+ * @Last modified time: 2017-05-19T11:54:52+10:00
  */
 
-import cleanup from './cleanup.workflow';
-
 export default (app, cwd, helpers) => {
-  return helpers
-    .exec('yarn test')
-    .then(() => cleanup(app, cwd, helpers))
-    .catch(() => cleanup(app, cwd, helpers).then(() => Promise.reject(1)));
+  return helpers.exec('yarn test');
 };
