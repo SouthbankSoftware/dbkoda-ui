c4d7ffac016960a90dd7271fcea5ad0e1a8df49d
Guy Harrison <guy.a.harrison@gmail.com>
2017-05-24 16:58:35 +1000

    Top Collections detail screen

---
+++ src/components/DetailsPanel/Functions/TopCollections.js
@@ -0,0 +1,17 @@
+/**
+ * @Author: Wahaj Shamim <wahaj>
+ * @Date:   2017-05-22T15:30:25+10:00
+ * @Email:  wahaj@southbanksoftware.com
+ * @Last modified by:   wahaj
+ * @Last modified time: 2017-05-23T13:26:55+10:00
+ */
+
+export const TopCollections = {
+  // Prefill function for alter user
+  dbenvy_TopCollections: () => {
+    return 'dbe.Top()';
+  },
+  dbenvy_TopCollections_parse: (data) => {
+    return data;
+  }
+};
