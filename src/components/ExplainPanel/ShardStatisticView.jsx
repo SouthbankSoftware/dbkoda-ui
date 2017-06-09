d27006f9a62cb1ebc0eb27be378bc82ad240cc34
Yi Zhao <zhaoyi0113@gmail.com>
2017-06-09 10:21:24 +1000

    update shard explain execution time

--- src/components/ExplainPanel/ShardStatisticView.jsx
+++ src/components/ExplainPanel/ShardStatisticView.jsx
@@ -6,15 +6,24 @@
 import React from 'react';
 import './style.scss';
 
-
 const getAllShardStatistics = (explains) => {
   const shards = explains.executionStats.executionStages.shards;
   const allShards = [];
   shards.map((shard) => {
     const oneShard = [];
     let cursor = shard.executionStages;
+    let executionTime = 0;
     while (cursor) {
       oneShard.push(cursor);
+      let time = cursor.executionTimeMillisEstimate;
+      if (time === undefined) {
+        time = cursor.executionTimeMillis;
+      } else if (cursor.executionTimeMillis !== undefined) {
+        time = Math.max(time, cursor.executionTimeMillis);
+      }
+      if (time > executionTime) {
+        executionTime = time;
+      }
       if (!cursor.inputStage && cursor.inputStages && cursor.inputStages.constructor === Array) {
         let examined = 0;
         let nReturned = 0;
@@ -29,6 +38,7 @@ const getAllShardStatistics = (explains) => {
     // get the executionTimeMillisEstimate and nReturned from the first child, docsExamined from the deepest child
     oneShard[0].docsExamined = oneShard[oneShard.length - 1].docsExamined ? oneShard[oneShard.length - 1].docsExamined : oneShard[oneShard.length - 1].keysExamined;
     oneShard[0].shardName = shard.shardName;
+    oneShard[0].executionTime = executionTime;
     allShards.push(oneShard);
   });
   return allShards;
@@ -61,7 +71,7 @@ export default ({explains}) => {
           <div className="cell">{shard.shardName}</div>
           <div className="cell">{shard.docsExamined}</div>
           <div className="cell">{shard.nReturned}</div>
-          <div className="cell">{shard.executionTimeMillisEstimate}</div>
+          <div className="cell">{shard.executionTime}</div>
         </div>);
       })
     }
--- src/components/ExplainPanel/Utils.js
+++ src/components/ExplainPanel/Utils.js
@@ -2,3 +2,39 @@
  * Created by joey on 6/6/17.
  */
 
+
+export const getWorstStage = (stages) => {
+  let max = 0;
+  stages.map((stage) => {
+    let time = stage.executionTimeMillisEstimate;
+    if (time === undefined) {
+      time = stage.executionTimeMillis;
+    } else if (stage.executionTimeMillis !== undefined) {
+      time = Math.max(time, stage.executionTimeMillis);
+    }
+    if (max < time) {
+      max = time;
+    }
+  });
+  return max;
+};
+
+export const getWorstShardStages = (shards) => {
+  let max = -1;
+  let worst = null;
+  let shardName = null;
+  shards.map((shard) => {
+    if (shard && shard.stages && shard.stages.length > 0) {
+      if (worst === null) {
+        worst = shard.stages;
+      }
+      const worstStageTime = getWorstStage(shard.stages);
+      if (max < worstStageTime) {
+        max = worstStageTime;
+        shardName = shard.shardName;
+        worst = shard.stages;
+      }
+    }
+  });
+  return {shardName, worst};
+};
