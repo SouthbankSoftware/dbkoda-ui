6a5fdff80ff947ffc8d16280e0c1ac9787c1308d
Guy Harrison <guy.a.harrison@gmail.com>
2017-06-16 19:08:32 -0700

    rs.status, sh.status and rs.stepdown support

---
+++ src/components/TreeActionPanel/Functions/rsstepdown.js
@@ -0,0 +1,14 @@
+/**
+ * @Author: Wahaj Shamim <wahaj>
+ * @Date:   2017-04-03T16:14:52+10:00
+ * @Email:  wahaj@southbanksoftware.com
+ * @Last modified by:   wahaj
+ * @Last modified time: 2017-05-03T12:12:33+10:00
+ */
+
+export const rsstepdown = {
+    executeCommand: null,
+    setExecuteFunction: (cbFuncExecute) => {
+        Aggregate.executeCommand = cbFuncExecute;
+    }
+};
