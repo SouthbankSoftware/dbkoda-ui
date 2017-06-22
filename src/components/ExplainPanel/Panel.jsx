b91b5d294d5a6118f6ba277d30406146f1de740c
michaeljharrison <mike@southbanksoftware.com>
2017-06-21 19:02:39 -0500

    Updated explain plan warning.

--- src/components/ExplainPanel/Panel.jsx
+++ src/components/ExplainPanel/Panel.jsx
@@ -18,36 +18,41 @@ import ExplainView from './ExplainView';
 import QueryCommandView from './QueryCommandView';
 
 export const Header = ({viewType, switchExplainView}) => {
-  return (<div className="explain-header">
-    <span className="explain-label">
-      {globalString('explain/heading')}
-    </span>
-    <Button
-      className="pt-label explain-view-switch-button"
-      onClick={switchExplainView}>
-      {
-        viewType === 0 ?
-          globalString('explain/panel/rawView') :
-          globalString('explain/panel/explainView')
-      }
-    </Button>
-  </div>);
+  return (
+    <div className="explain-header">
+      <span className="explain-label">
+        {globalString('explain/heading')}
+      </span>
+      <Button
+        className="pt-label explain-view-switch-button"
+        onClick={switchExplainView}>
+        {viewType === 0
+          ? globalString('explain/panel/rawView')
+          : globalString('explain/panel/explainView')
+}
+      </Button>
+    </div>
+  );
 };
 
 const Panel = ({editor, switchExplainView, viewType}) => {
   if (editor.explains && editor.explains.error) {
-    return (<div className="explain-error-panel">
-      <div className="header">Failed to parse explain output</div>
-      <QueryCommandView command={editor.explains.command} />
-      <div className="output">{editor.explains.output}</div>
-    </div>);
+    return (
+      <div className="explain-error-panel">
+        <div className="header">Failed to parse explain output, <b>make sure to highlight entire statement.</b></div>
+        <QueryCommandView command={editor.explains.command} />
+        <div className="output">{editor.explains.output}</div>
+      </div>
+    );
   }
-  return (<div className="explain-panel">
-    <Header switchExplainView={switchExplainView} viewType={viewType} />
-    {
-      viewType === 0 ? <ExplainView explains={editor.explains} /> : <RawJson explains={editor.explains} />
-    }
-  </div>);
+  return (
+    <div className="explain-panel">
+      <Header switchExplainView={switchExplainView} viewType={viewType} /> {viewType === 0
+        ? <ExplainView explains={editor.explains} />
+        : <RawJson explains={editor.explains} />
+}
+    </div>
+  );
 };
 
 export default Panel;
