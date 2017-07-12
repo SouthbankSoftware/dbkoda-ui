5b1059a4da1c5c1e77f6382995de3a541706cf89
michaeljharrison <michael.j.harrison@outlook.com>
2017-07-13 09:48:38 +1000

    Cleaned Up Welcome Page Styling.

--- src/stores/global.js
+++ src/stores/global.js
@@ -121,7 +121,7 @@ export default class Store {
   layout = {
     alertIsLoading: false,
     optInVisible: true,
-    overallSplitPos: '40%',
+    overallSplitPos: '30%',
     leftSplitPos: '50%',
     rightSplitPos: '60%'
   };
