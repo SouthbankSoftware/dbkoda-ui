7be658b9e4c136dbf4f481535d7d7e8da88087f7
Wahaj Shamim <wahaj@southbanksoftware.com>
2017-07-17 15:08:08 +1000

    Added copyright header script
    Script to add copyright header to all the code files

---
+++ copyright_header.sh
@@ -0,0 +1,9 @@
+# @Author: Wahaj Shamim <wahaj>
+# @Date:   2017-07-17T11:47:09+10:00
+# @Email:  wahaj@southbanksoftware.com
+# @Last modified by:   wahaj
+# @Last modified time: 2017-07-17T13:37:56+10:00
+
+
+
+copyright-header --add-path src/ --license AGPL3 --copyright-holder 'Southbank Software' --copyright-software 'dbKoda' --copyright-software-description 'a modern, open source code editor, for MongoDB.' --copyright-year 2017-2018 --output-dir .
