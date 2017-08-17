ffc16f25e3c5ec3ffe185c68bf5dfa340c7b235d
Guy Harrison <guy.a.harrison@gmail.com>
2017-08-17 12:12:27 +1000

    Bucket & sortbycount

--- src/components/AggregateViews/AggregateBlocks/BlockFunctions/SortByCount.js
+++ src/components/AggregateViews/AggregateBlocks/BlockFunctions/SortByCount.js
@@ -21,4 +21,7 @@ import * as common from '../../../TreeActionPanel/Functions/Common.js';
 
 export const SortByCount = {
   dbkodaListAttributes: common.dbkodaListAttributes,
+  dbkodaListAttributesAgg: common.dbkodaListAttributesAgg,
+  dbkodaListAttributes_parse: common.dbkodaListAttributes_parse,
+  dbkodaListAttributesAgg_parse: common.dbkodaListAttributesAgg_parse,
 };
