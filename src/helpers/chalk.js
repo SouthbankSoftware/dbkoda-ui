fc24448310a6e8678937644fd3a33acd75c4afdd
Guan Gui <guan@southbanksoftware.com>
2017-11-15 14:41:12 +1100

    Handled xterm demo backend not available exception

---
+++ src/helpers/chalk.js
@@ -0,0 +1,29 @@
+/**
+ * @Author: Guan Gui <guiguan>
+ * @Date:   2017-11-15T14:27:21+11:00
+ * @Email:  root@guiguan.net
+ * @Last modified by:   guiguan
+ * @Last modified time: 2017-11-15T14:28:29+11:00
+ *
+ * dbKoda - a modern, open source code editor, for MongoDB.
+ * Copyright (C) 2017-2018 Southbank Software
+ *
+ * This file is part of dbKoda.
+ *
+ * dbKoda is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * dbKoda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with dbKoda.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import chalk from 'chalk';
+
+export default new chalk.constructor({ enabled: true, level: 3 });
