367a78222f397c9af44f42cf3f498d771d799b99
Guan Gui <guan@southbanksoftware.com>
2017-11-17 16:23:31 +1100

    Fixed production ui building issue

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,16 +1,17 @@
 /**
  * @Last modified by:   guiguan
- * @Last modified time: 2017-06-09T06:36:28+10:00
+ * @Last modified time: 2017-11-17T16:09:01+11:00
  */
 
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./common');
 
 module.exports = merge.strategy({
   'module.rules': 'prepend',
-  plugins: 'prepend'
+  plugins: 'prepend',
 })(common, {
   module: {
     rules: [
@@ -18,25 +19,33 @@ module.exports = merge.strategy({
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader'
-        })
+          use: 'css-loader',
+        }),
       },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
-    ]
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
+    ],
   },
   devtool: 'cheap-source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
-    new ExtractTextPlugin('style.css')
-  ]
+    new ExtractTextPlugin('style.css'),
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        beautify: false,
+        ecma: 6,
+        compress: true,
+        comments: false,
+      },
+    }),
+  ],
 });
