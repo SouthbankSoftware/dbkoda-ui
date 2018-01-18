5d03a71f4d1c47c52dd98dd15cafdc3f233e0609
Wahaj Shamim <wahaj@southbanksoftware.com>
2018-01-18 17:04:00 +1100

    Refactoring Connection Panel

--- src/api/Profile.js
+++ src/api/Profile.js
@@ -2,24 +2,251 @@
  * @Author: Wahaj Shamim <wahaj>
  * @Date:   2017-07-31T13:06:24+10:00
  * @Email:  wahaj@southbanksoftware.com
- * @Last modified by:   guiguan
- * @Last modified time: 2017-10-30T16:04:21+11:00
+ * @Last modified by:   wahaj
+ * @Last modified time: 2018-01-18T15:54:19+11:00
  */
 
 import { action, observable } from 'mobx';
 import uuidV1 from 'uuid';
 import { EditorTypes } from '#/common/Constants';
+import { ProfileForm } from '#/ConnectionPanel/ProfileForm';
+import { featherClient } from '~/helpers/feathers';
+import { Broker, EventType } from '~/helpers/broker';
+import EventLogging from '#/common/logging/EventLogging';
+import { ProfileStatus, DrawerPanes } from '#/common/Constants';
 import StaticApi from './static';
+
 
 export default class ProfileApi {
   store;
   api;
+  config;
   profileStore;
+  toasterCallback;
 
-  constructor(store, api, profileStore) {
+  constructor(store, api, profileStore, config) {
     this.store = store;
     this.api = api;
+    this.config = config;
     this.profileStore = profileStore;
+    // this.profiles = profileStore.profiles;
+    // this.profileList = store.profileList;
+
+    this.setToasterCallback = this.setToasterCallback.bind(this);
+    this.validateConnectionFormData = this.validateConnectionFormData.bind(this);
+    this.connectProfile = this.connectProfile.bind(this);
+    this.onFail = this.onFail.bind(this);
+    this.onSuccess = this.onSuccess.bind(this);
+    this.closeConnectionPane = this.closeConnectionPane.bind(this);
+    this.saveProfile = this.saveProfile.bind(this);
+  }
+
+  setToasterCallback(fn) {
+    this.toasterCallback = fn;
+  }
+
+  /**
+   * validate connection form data
+   *
+   * @param data
+   * @returns {boolean}
+   */
+  validateConnectionFormData(data) {
+    const { profiles } = this.profileStore;
+    let validate = true;
+    profiles.forEach((value) => {
+      if (value.alias === data.alias) {
+        this.toasterCallback && this.toasterCallback('existingAlias');
+        validate = false;
+      }
+    });
+    return validate;
+  }
+
+  @action
+  async connectProfile(data) {
+    const { profileList } = this.store;
+    const { selectedProfile } = profileList;
+    let edit = false;
+    if (selectedProfile) {
+      edit = true;
+    }
+    if (!edit && !this.validateConnectionFormData(data)) {
+      return Promise.reject(globalString('connection/validationError'));
+    }
+    const query = {};
+    let connectionUrl;
+    if (data.hostRadio) {
+      connectionUrl = ProfileForm.mongoProtocol + data.host + ':' + data.port;
+    } else if (data.urlRadio) {
+      connectionUrl = data.url;
+    }
+    if (data.ssh) {
+      query.ssh = data.ssh;
+      query.sshTunnel = data.sshTunnel;
+      query.remoteHost = data.host;
+      query.remotePort = data.port;
+      query.sshHost = data.remoteHost;
+      query.remoteUser = data.remoteUser;
+      query.localHost = '127.0.0.1';
+      data.sshLocalPort = await ProfileForm.getRandomPort();
+      query.localPort = data.sshLocalPort;
+      if (data.sshTunnel) {
+        connectionUrl =
+          ProfileForm.mongoProtocol + query.localHost + ':' + query.localPort;
+      }
+      if (data.passRadio) {
+        query.remotePass = data.remotePass;
+      } else if (data.keyRadio) {
+        query.sshKeyFile = data.sshKeyFile;
+        query.passPhrase = data.passPhrase;
+      }
+    }
+    if (data.sha) {
+      query.username = data.username;
+      query.password = data.password;
+      query.password = data.password;
+      query.authenticationDatabase = data.authenticationDatabase;
+    }
+    if (data.ssl) {
+      connectionUrl.indexOf('?') > 0
+        ? (connectionUrl += '&ssl=true')
+        : (connectionUrl += '?ssl=true');
+    }
+
+    query.database = data.database;
+    query.url = connectionUrl;
+    query.ssl = data.ssl;
+    query.sslAllowInvalidCertificates = data.sslAllowInvalidCertificates;
+    query.test = data.test;
+    query.authorization = data.authorization;
+    if (selectedProfile) {
+      query.id = selectedProfile.id;
+      query.shellId = selectedProfile.shellId;
+    }
+
+    profileList.creatingNewProfile = true;
+    const service = featherClient().service('/mongo-connection');
+    service.timeout = 30000;
+    return service
+      .create({}, { query })
+      .then((res) => {
+        this.onSuccess(res, data);
+      })
+      .catch((err) => {
+        console.error(err);
+        this.onFail();
+        this.toasterCallback && this.toasterCallback('connectionFail', err);
+      });
+  }
+
+  @action
+  onFail() {
+    const { profileList } = this.store;
+    if (this.config.settings.telemetryEnabled) {
+      EventLogging.recordManualEvent(
+        EventLogging.getTypeEnum().EVENT.CONNECTION_PANEL.NEW_PROFILE.FAILED,
+        EventLogging.getFragmentEnum().PROFILES,
+        globalString('connection/createProfileError')
+      );
+    }
+    profileList.creatingNewProfile = false;
+  }
+
+  /**
+   * when connection successfully created, this method will add the new profile on store.
+   */
+   @action
+  onSuccess(res, data) {
+    const { profileList } = this.store;
+    const { profiles } = this.profileStore;
+    const { selectedProfile } = profileList;
+    let edit = false;
+    if (selectedProfile) {
+      edit = true;
+    }
+    profileList.creatingNewProfile = false;
+    if (!data.test) {
+      if (edit) {
+        profiles.delete(selectedProfile.id);
+        Broker.emit(EventType.createShellOutputEvent(res.id, res.shellId), {
+          id: res.id,
+          shellId: res.shellId,
+          output: res.output.join('\n'),
+          mongoType: res.mongoType
+        });
+      }
+      const profile = {
+        id: res.id,
+        shellId: res.shellId,
+        password: null,
+        status: ProfileStatus.OPEN,
+        database: data.database,
+        authenticationDatabase: data.authenticationDatabase,
+        alias: data.alias,
+        authorization: data.authorization,
+        host: data.host,
+        hostRadio: data.hostRadio,
+        port: data.port,
+        ssl: data.ssl,
+        sslAllowInvalidCertificates: data.sslAllowInvalidCertificates,
+        test: data.test,
+        url: data.url,
+        urlRadio: data.urlRadio,
+        username: data.username,
+        sha: data.sha,
+        ssh: data.ssh,
+        sshTunnel: data.sshTunnel,
+        remoteHost: data.remoteHost,
+        remoteUser: data.remoteUser,
+        sshLocalPort: data.sshLocalPort,
+        passRadio: data.passRadio,
+        keyRadio: data.keyRadio,
+        sshKeyFile: data.sshKeyFile,
+        initialMsg: res.output ? res.output.join('\r') : '',
+        dbVersion: res.dbVersion,
+        shellVersion: res.shellVersion,
+        mongoType: res.mongoType
+      };
+      console.debug('profile:', profile);
+      if ((data.passPhrase && data.passPhrase != '') || data.bPassPhrase) {
+        profile.bPassPhrase = true;
+      }
+      if ((data.remotePass && data.remotePass != '') || data.bRemotePass) {
+        profile.bRemotePass = true;
+      }
+      profiles.set(res.id, profile);
+      profileList.selectedProfile = profiles.get(res.id);
+      this.closeConnectionPane();
+      Broker.emit(EventType.NEW_PROFILE_CREATED, profiles.get(res.id));
+    }
+    this.toasterCallback && this.toasterCallback('connectionSuccess');
+  }
+  @action
+  closeConnectionPane() {
+    this.store.setDrawerChild(DrawerPanes.DEFAULT);
+  }
+  @action
+  saveProfile(formData) {
+    const { profileList } = this.store;
+    const { profiles } = this.profileStore;
+    const { selectedProfile } = profileList;
+    const edit = (selectedProfile !== undefined && selectedProfile !== null);
+
+    const profile = { ...formData, status: ProfileStatus.CLOSED };
+    if (edit) {
+      profile.id = selectedProfile.id;
+      profile.shellId = selectedProfile.shellId;
+      profiles.delete(profile.id);
+    }
+    if (!profile.id) {
+      profile.id = uuidV1();
+    }
+    if (!profile.shellId) {
+      profile.shellId = uuidV1();
+    }
+    profiles.set(profile.id, profile);
+    this.closeConnectionPane();
   }
 
   /**
--- src/components/ConnectionPanel/ConnectionPanel.jsx
+++ src/components/ConnectionPanel/ConnectionPanel.jsx
@@ -21,251 +21,69 @@
  * @Author: Wahaj Shamim <wahaj>
  * @Date:   2017-03-30T09:57:22+11:00
  * @Email:  wahaj@southbanksoftware.com
- * @Last modified by:   guiguan
- * @Last modified time: 2017-10-30T14:10:52+11:00
+ * @Last modified by:   wahaj
+ * @Last modified time: 2018-01-18T13:54:59+11:00
  */
 
 /**
  * create new profile form and handle connection
  */
 import React from 'react';
-import { action } from 'mobx';
 import { inject, observer } from 'mobx-react';
-import EventLogging from '#/common/logging/EventLogging';
 import { Position } from '@blueprintjs/core';
-import uuidV1 from 'uuid/v1';
-import { createForm, ProfileForm } from './ProfileForm';
+import { createForm } from './ProfileForm';
 import Panel from './Panel';
-import { featherClient } from '../../helpers/feathers';
 import { DBKodaToaster } from '../common/Toaster';
-import { Broker, EventType } from '../../helpers/broker';
-import { ProfileStatus, DrawerPanes } from '.././common/Constants';
 
 const ConnectionPanel = ({
+  api,
   profiles,
-  profileList,
-  setDrawerChild,
-  settings
+  profileList
 }) => {
-  const selectedProfile = profileList.selectedProfile;
+  const { selectedProfile } = profileList;
   let edit = false;
   if (profileList.selectedProfile) {
     edit = true;
   }
   const form = createForm(selectedProfile);
 
-  /**
-   * validate connection form data
-   *
-   * @param data
-   * @returns {boolean}
-   */
-  const validateConnectionFormData = (data) => {
-    let validate = true;
-    profiles.forEach((value) => {
-      if (value.alias === data.alias) {
+  const showToaster = (strErrorCode, err) => {
+    switch (strErrorCode) {
+      case 'existingAlias':
         DBKodaToaster(Position.LEFT_BOTTOM).show({
           message: globalString('connection/existingAlias'),
           className: 'danger',
           iconName: 'pt-icon-thumbs-down'
         });
-        validate = false;
-      }
-    });
-    return validate;
-  };
-
-  const close = action(() => {
-    setDrawerChild(DrawerPanes.DEFAULT);
-  });
-  /**
-   * when connection successfully created, this method will add the new profile on store.
-   */
-  const onSuccess = action((res, data) => {
-    profileList.creatingNewProfile = false;
-    let message = globalString('connection/success');
-    let position = Position.LEFT_BOTTOM;
-    if (!data.test) {
-      if (edit) {
-        profiles.delete(selectedProfile.id);
-        Broker.emit(EventType.createShellOutputEvent(res.id, res.shellId), {
-          id: res.id,
-          shellId: res.shellId,
-          output: res.output.join('\n'),
-          mongoType: res.mongoType
-        });
-      }
-      position = Position.RIGHT_TOP;
-      form.reset();
-      const profile = {
-        id: res.id,
-        shellId: res.shellId,
-        password: null,
-        status: ProfileStatus.OPEN,
-        database: data.database,
-        authenticationDatabase: data.authenticationDatabase,
-        alias: data.alias,
-        authorization: data.authorization,
-        host: data.host,
-        hostRadio: data.hostRadio,
-        port: data.port,
-        ssl: data.ssl,
-        sslAllowInvalidCertificates: data.sslAllowInvalidCertificates,
-        test: data.test,
-        url: data.url,
-        urlRadio: data.urlRadio,
-        username: data.username,
-        sha: data.sha,
-        ssh: data.ssh,
-        sshTunnel: data.sshTunnel,
-        remoteHost: data.remoteHost,
-        remoteUser: data.remoteUser,
-        sshLocalPort: data.sshLocalPort,
-        passRadio: data.passRadio,
-        keyRadio: data.keyRadio,
-        sshKeyFile: data.sshKeyFile,
-        initialMsg: res.output ? res.output.join('\r') : '',
-        dbVersion: res.dbVersion,
-        shellVersion: res.shellVersion,
-        mongoType: res.mongoType
-      };
-      console.debug('profile:', profile);
-      if ((data.passPhrase && data.passPhrase != '') || data.bPassPhrase) {
-        profile.bPassPhrase = true;
-      }
-      if ((data.remotePass && data.remotePass != '') || data.bRemotePass) {
-        profile.bRemotePass = true;
-      }
-      profiles.set(res.id, profile);
-      profileList.selectedProfile = profiles.get(res.id);
-      close();
-      Broker.emit(EventType.NEW_PROFILE_CREATED, profiles.get(res.id));
-    } else {
-      message = globalString('connection/success');
-    }
-    DBKodaToaster(position).show({
-      message,
-      className: 'success',
-      iconName: 'pt-icon-thumbs-up'
-    });
-  });
-
-  const onFail = action(() => {
-    if (settings.telemetryEnabled) {
-      EventLogging.recordManualEvent(
-        EventLogging.getTypeEnum().EVENT.CONNECTION_PANEL.NEW_PROFILE.FAILED,
-        EventLogging.getFragmentEnum().PROFILES,
-        globalString('connection/createProfileError')
-      );
-    }
-    profileList.creatingNewProfile = false;
-  });
-
-  const connect = action(async (data) => {
-    if (!edit && !validateConnectionFormData(data)) {
-      return Promise.reject(globalString('connection/validationError'));
-    }
-    const query = {};
-    let connectionUrl;
-    if (data.hostRadio) {
-      connectionUrl = ProfileForm.mongoProtocol + data.host + ':' + data.port;
-    } else if (data.urlRadio) {
-      connectionUrl = data.url;
-    }
-    if (data.ssh) {
-      query.ssh = data.ssh;
-      query.sshTunnel = data.sshTunnel;
-      query.remoteHost = data.host;
-      query.remotePort = data.port;
-      query.sshHost = data.remoteHost;
-      query.remoteUser = data.remoteUser;
-      query.localHost = '127.0.0.1';
-      data.sshLocalPort = await ProfileForm.getRandomPort();
-      query.localPort = data.sshLocalPort;
-      if (data.sshTunnel) {
-        connectionUrl =
-          ProfileForm.mongoProtocol + query.localHost + ':' + query.localPort;
-      }
-      if (data.passRadio) {
-        query.remotePass = data.remotePass;
-      } else if (data.keyRadio) {
-        query.sshKeyFile = data.sshKeyFile;
-        query.passPhrase = data.passPhrase;
-      }
-    }
-    if (data.sha) {
-      query.username = data.username;
-      query.password = data.password;
-      query.password = data.password;
-      query.authenticationDatabase = data.authenticationDatabase;
-    }
-    if (data.ssl) {
-      connectionUrl.indexOf('?') > 0
-        ? (connectionUrl += '&ssl=true')
-        : (connectionUrl += '?ssl=true');
-    }
-
-    query.database = data.database;
-    query.url = connectionUrl;
-    query.ssl = data.ssl;
-    query.sslAllowInvalidCertificates = data.sslAllowInvalidCertificates;
-    query.test = data.test;
-    query.authorization = data.authorization;
-    if (selectedProfile) {
-      query.id = selectedProfile.id;
-      query.shellId = selectedProfile.shellId;
-    }
-
-    profileList.creatingNewProfile = true;
-    const service = featherClient().service('/mongo-connection');
-    service.timeout = 30000;
-    return service
-      .create({}, { query })
-      .then((res) => {
-        onSuccess(res, data);
-      })
-      .catch((err) => {
-        console.error(err);
-        onFail();
+      break;
+      case 'connectionFail':
         DBKodaToaster(Position.LEFT_BOTTOM).show({
-          message: (
-            <span
-              dangerouslySetInnerHTML={{
-                __html: 'Error: ' + err.message.substring(0, 256) + '...'
-              }}
-            />
-          ), // eslint-disable-line react/no-danger
+          message: (<span dangerouslySetInnerHTML={{ __html: 'Error: ' + err.message.substring(0, 256) + '...' }} />), // eslint-disable-line react/no-danger
           className: 'danger',
           iconName: 'pt-icon-thumbs-down'
         });
-      });
-  });
-
-  const save = action((formData) => {
-    const profile = { ...formData, status: ProfileStatus.CLOSED };
-    if (edit) {
-      profile.id = selectedProfile.id;
-      profile.shellId = selectedProfile.shellId;
-      profiles.delete(profile.id);
+      break;
+      case 'connectionSuccess':
+        DBKodaToaster(Position.RIGHT_TOP).show({
+          message: globalString('connection/success'),
+          className: 'success',
+          iconName: 'pt-icon-thumbs-up'
+        });
+      break;
+      default:
+      break;
     }
-    if (!profile.id) {
-      profile.id = uuidV1();
-    }
-    if (!profile.shellId) {
-      profile.shellId = uuidV1();
-    }
-    profiles.set(profile.id, profile);
-    close();
-  });
+  };
+  api.setToasterCallback(showToaster);
 
   return (
     <Panel
       form={form}
-      close={close}
+      close={api.closeConnectionPane}
       edit={edit}
-      connect={connect}
+      connect={api.connectProfile}
       profiles={profiles}
-      save={save}
+      save={api.saveProfile}
       title={
         edit
           ? globalString('connection/editHeading')
@@ -276,9 +94,8 @@ const ConnectionPanel = ({
 };
 
 export default inject(allStores => ({
+  api: allStores.api,
   store: allStores.store,
   profiles: allStores.profileStore.profiles,
-  profileList: allStores.store.profileList,
-  setDrawerChild: allStores.store.setDrawerChild,
-  settings: allStores.config.settings
+  profileList: allStores.store.profileList
 }))(observer(ConnectionPanel));
