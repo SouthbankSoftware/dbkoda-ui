3bdc02f5fdcd74f4f5ea5dda3e32a0e00807463c
Guan Gui <guan@southbanksoftware.com>
2018-02-21 15:43:05 +1100

    Inited AlarmView

---
+++ src/components/PerformancePanel/Widgets/Widget/AlarmView.jsx
@@ -0,0 +1,39 @@
+/**
+ * @flow
+ *
+ * @Author: Guan Gui <guiguan>
+ * @Date:   2018-02-21T14:36:12+11:00
+ * @Email:  root@guiguan.net
+ * @Last modified by:   guiguan
+ * @Last modified time: 2018-02-21T15:42:12+11:00
+ *
+ * dbKoda - a modern, open source code editor, for MongoDB.
+ * Copyright (C) 2017-2018 Southbank Software
+ *
+ * This file is part of dbKoda.
+ *
+ * dbKoda is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * dbKoda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with dbKoda.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as React from 'react';
+// import _ from 'lodash';
+// import styles from './AlarmView.scss';
+
+type Props = {};
+
+export default class AlarmView extends React.PureComponent<Props> {
+  render() {
+    return <div>This will be rendered as a super cool alarm view. Please stay tuned</div>;
+  }
+}
