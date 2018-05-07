c21f3e711654e66cbd149d92de03fb675190b316
Wahaj Shamim <wahaj@southbanksoftware.com>
2018-05-07 12:56:16 +1000

    Fix issues with Top Connections Panel Explain
    Added support for Update and Delete OP

--- src/components/TopConnectionsPanel/Views/OperationDetails.jsx
+++ src/components/TopConnectionsPanel/Views/OperationDetails.jsx
@@ -109,8 +109,8 @@ export default class OperationDetails extends React.Component {
   }
 
   render() {
-    // const { topConnectionsPanel } = this.props;
-    // //                 loading={topConnectionsPanel.bLoadingExplain}
+    const { topConnectionsPanel } = this.props;
+
     return (
       <div style={{ height: '100%' }}>
         <nav className="pt-navbar actionsToolbar">
@@ -120,7 +120,7 @@ export default class OperationDetails extends React.Component {
           <div className="pt-navbar-group pt-align-right">
             <Tooltip
               className="ResetButton pt-tooltip-indicator pt-tooltip-indicator-form"
-              content="Toggle Explain Plan"
+              content="Fetch Explain Plan"
               hoverOpenDelay={1000}
               inline
               intent={Intent.PRIMARY}
@@ -129,6 +129,7 @@ export default class OperationDetails extends React.Component {
               <Button
                 className="reset-button pt-button pt-intent-primary"
                 text="Explain"
+                loading={topConnectionsPanel.bLoadingExplain}
                 onClick={this.getExplainForSelectedOp}
               />
             </Tooltip>
--- src/window/stores/performance.js
+++ src/window/stores/performance.js
@@ -136,23 +136,60 @@ class PerformanceWindowApi {
         }
         return false;
       };
+      const cmdNotSupported = () => {
+        topConnectionsPanel.lastExplainId = null;
+        topConnectionsPanel.bLoadingExplain = false;
+        this.showToaster({
+          message: 'Command not supported for Explain.',
+          className: 'danger',
+          iconName: 'pt-icon-thumbs-down'
+        });
+      };
       const cmdForExplain = selectedOperation.command;
-      if (selectedOperation.command && selectedOperation.command.$db) {
+      let explainCommandStr = '';
+      let databaseStr = '';
+      if (
+        (selectedOperation.op === 'command' || selectedOperation.op === 'query') &&
+        selectedOperation.command &&
+        selectedOperation.command.$db
+      ) {
         const database = _.pick(cmdForExplain, '$db');
+        databaseStr = database.$db;
         let commandParams = _.omit(cmdForExplain, '$db');
         const command = _.pickBy(commandParams, filterCommand);
-        const commandStr = JSON.stringify(command);
-        commandParams = _.omitBy(commandParams, filterCommand);
-        const commandParamsStr = JSON.stringify(commandParams);
-        const explainCommandStr =
-          commandParamsStr.substr(0, 1) +
-          commandStr.substr(1, commandStr.length - 2) +
-          ',' +
-          commandParamsStr.substr(1);
+        if (Object.keys(command).length > 0) {
+          const commandStr = JSON.stringify(command);
+          commandParams = _.omitBy(commandParams, filterCommand);
+          const commandParamsStr = JSON.stringify(commandParams);
+          explainCommandStr =
+            commandParamsStr.substr(0, 1) +
+            commandStr.substr(1, commandStr.length - 2) +
+            ',' +
+            commandParamsStr.substr(1);
+        } else {
+          cmdNotSupported();
+        }
+      } else if (selectedOperation.op === 'update') {
+        const nsParams = selectedOperation.ns.split('.');
+        [databaseStr] = nsParams;
+        const [, collection] = nsParams;
+        const commandStr = JSON.stringify(selectedOperation.command);
+        explainCommandStr = `{"update": "${collection}", "updates":[${commandStr}]}`;
+      } else if (selectedOperation.op === 'remove') {
+        const nsParams = selectedOperation.ns.split('.');
+        [databaseStr] = nsParams;
+        const [, collection] = nsParams;
+        const commandStr = JSON.stringify(selectedOperation.command);
+        explainCommandStr = `{"delete": "${collection}", "deletes":[${commandStr}]}`;
+      } else {
+        cmdNotSupported();
+      }
+
+      if (explainCommandStr.length > 0) {
         this.sendCommandToMainProcess('pw_getOperationExplainPlan', {
           explainId,
           explainCmd: explainCommandStr,
-          database: database.$db
+          database: databaseStr
         });
       }
     }
@@ -351,8 +388,9 @@ export default class Store {
           }
         } else if (args.command === 'mw_explainForOperation') {
           console.log(args.payload);
+          this.topConnectionsPanel.bLoadingExplain = false;
           if (args.explainId && args.explainId === this.topConnectionsPanel.lastExplainId) {
-            if (args.payload.executionStats) {
+            if (args.payload && args.payload.executionStats) {
               extendObservable(this.topConnectionsPanel.selectedOperation, {
                 execStats: args.payload.executionStats.executionStages
               });
