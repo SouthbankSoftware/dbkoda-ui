008418cbce90d41b8e5f6f2f3805dc88836756ea
Wahaj Shamim <wahaj@southbanksoftware.com>
2018-05-18 14:16:40 +1000

    Minor exception fix

--- src/components/ProfileManager/JsonForm.js
+++ src/components/ProfileManager/JsonForm.js
@@ -3,7 +3,7 @@
  * @Date:   2018-01-24T09:50:36+11:00
  * @Email:  inbox.wahaj@gmail.com
  * @Last modified by:   wahaj
- * @Last modified time: 2018-04-20T11:48:20+10:00
+ * @Last modified time: 2018-05-18T12:17:59+10:00
  *
  * dbKoda - a modern, open source code editor, for MongoDB.
  * Copyright (C) 2017-2018 Southbank Software
@@ -32,7 +32,17 @@ export const FieldBindings = {
   text: ['name', 'value', 'type', 'id', 'placeholder', 'disabled', 'onChange', 'onBlur'],
   password: ['name', 'value', 'type', 'id', 'placeholder', 'disabled', 'onChange', 'onBlur'],
   number: ['name', 'value', 'type', 'id', 'placeholder', 'disabled', 'onValueChange', 'onBlur'],
-  checkbox: ['name', 'value', 'type', 'id', 'placeholder', 'disabled', 'onClick', 'onBlur'],
+  checkbox: [
+    'name',
+    'value',
+    'type',
+    'id',
+    'placeholder',
+    'disabled',
+    'onClick',
+    'onChange',
+    'onBlur'
+  ],
   file: ['name', 'value', 'id', 'placeholder', 'disabled', 'onChange', 'onBlur'],
   select: ['name', 'value', 'id', 'placeholder', 'disabled', 'onChange', 'onBlur']
 };
@@ -126,6 +136,9 @@ export class JsonForm {
     };
     set(field, { error: '' });
     field.onChange = action(e => {
+      if (e.currentTarget.type === 'checkbox') {
+        return;
+      }
       this.updateFieldValue(field, e.currentTarget.value);
     });
     field.onBlur = () => {
