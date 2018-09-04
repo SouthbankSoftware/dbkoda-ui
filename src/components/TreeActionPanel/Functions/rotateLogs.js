92a46fff1663fa29b77e9b743244eed8fbf88132
Guy Harrison <guy.a.harrison@gmail.com>
2018-09-04 13:56:24 +1000

    Rotate log action

---
+++ src/components/TreeActionPanel/Functions/rotateLogs.js
@@ -0,0 +1,36 @@
+/*
+ * dbKoda - a modern, open source code editor, for MongoDB.
+ * Copyright (C) 2017-2018 Southbank Software
+ *
+ * This file is part of dbKoda.
+ *
+ * dbKoda is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * dbKoda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with dbKoda.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @Author: Wahaj Shamim <wahaj>
+ * @Date:   2017-04-03T16:14:52+10:00
+ * @Email:  wahaj@southbanksoftware.com
+ * @Last modified by:   guiguan
+ * @Last modified time: 2017-12-15T13:42:45+11:00
+ */
+
+import { Aggregate } from './Aggregate';
+
+export const rotateLogs = {
+  executeCommand: null,
+  setExecuteFunction: cbFuncExecute => {
+    Aggregate.executeCommand = cbFuncExecute;
+  }
+};
